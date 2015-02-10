d4fa45b360ab3491c9b48dc497abce8b51ed3d91
Jamie M. Kass <jamie.m.kass@gmail.com>
2015-02-10 15:29:54 -0500

    changed layout to sidebar, added some ENMeval functionality
    Former-commit-id: 145a258b32352c82fba88fbb6a792b20edffb400

--- ui.R
+++ ui.R
@@ -5,8 +5,8 @@ shinyUI(fluidPage(
   titlePanel("ENM Interactive Evaluation with GBIF"),
   helpText("Calibrate and evaluate ENMs with GBIF data."),
   
-  fluidRow(
-    column(3, wellPanel(
+  sidebarLayout(
+    sidebarPanel(
       textInput("gbifName", label = "Enter scientific name of species", value = ''),
       actionButton("goName", "Submit name"),
       br(),
@@ -15,22 +15,38 @@ shinyUI(fluidPage(
                        actionButton("goMap", "Map points")),
       br(),
       conditionalPanel("input.goName",
-                         actionButton("goThin", "Run spThin")),
+                       numericInput("thinDist", label = "Thinning distance (km)", value = 25),
+                       actionButton("goThin", "Run spThin")),
       br(),
       conditionalPanel("input.goName",
-                         actionButton("goEval", "Run ENMeval"))
-      )),
-    
-    column(8,
-      conditionalPanel("input.goName",
-                       textOutput('GBIFtxt')),
-      conditionalPanel("input.goMap",
-                       textOutput('mapText')),
-      conditionalPanel("input.goThin",
-                       textOutput('thinText')),
-      br(),
-      conditionalPanel("input.goMap",
-                       plotOutput('GBIFmap'))
+                       checkboxGroupInput("fcs", label = "Select feature classes", 
+                                          choices = list("L" = "L", "LQ" = "LQ", "H" = "H",
+                                                         "LQH" = "LQH", "LQHP" = "LQHP", "LQHPT" = "LQHPT"),
+                                          selected = c("L", "LQ")),
+                       sliderInput("rms", label = "Select regularization multipliers",
+                                   min = 0, max = 10, value = c(1, 5)),
+                       numericInput("rmsBy", label = "RM step value", value = 1),
+                       selectInput("method", label = "Evaluation method",
+                                   choices = list("jackknife" = "jackknife", "block" = "block",
+                                                  "checkerboard1" = "checkerboard1", "checkerboard2" = "checkerboard2",
+                                                  "randomkfold" = "randomkfold", "user" = "user"), selected = "block"),
+                       actionButton("goEval", "Run ENMeval"))
+      ),
+  
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Map", 
+                 conditionalPanel("input.goName",
+                                  textOutput('GBIFtxt')),
+                 conditionalPanel("input.goMap",
+                                  textOutput('mapText')),
+                 conditionalPanel("input.goThin",
+                                  textOutput('thinText')),
+                 br(),
+                 plotOutput('GBIFmap')),
+        tabPanel("Occurrences", tableOutput('occTbl'))
+        #tabPanel("Plots", plotOutput())
+      )
     )
   )
 ))
