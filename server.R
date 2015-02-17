9c03b96758e81f2ed096ff7ce7dccf98a79d5d3c
tanuki <jamie.m.kass@gmail.com>
2015-02-17 15:35:06 -0500

    added backg selection and automatic mapping
    Former-commit-id: a51a16a7949b559c823c732d00e8463103e068c9

--- server.R
+++ server.R
@@ -9,10 +9,18 @@ library(rgbif)
 library(spThin)
 library(ENMeval)
 library(dismo)
+library(rgeos)
 library(ggplot2)
 
 #source("functions.R")
 
+mcp <- function (xy) {
+  xy <- as.data.frame(coordinates(xy))
+  coords.t <- chull(xy[, 1], xy[, 2])
+  xy.bord <- xy[coords.t, ]
+  xy.bord <- rbind(xy.bord[nrow(xy.bord), ], xy.bord)
+  return(SpatialPolygons(list(Polygons(list(Polygon(as.matrix(xy.bord))), 1))))
+}
 
 shinyServer(function(input, output, session) {
   
@@ -25,8 +33,8 @@ shinyServer(function(input, output, session) {
       withProgress(message = "Searching GBIF...", {
         results <- occ_search(scientificName = input$gbifName, limit = 50, 
                               fields = 'minimal', hasCoordinate = TRUE)
-        locs <- results$data[!is.na(results$data[,3]),][,c(4,3)]
-        names(locs) <- c('lon', 'lat')
+        locs <- results$data[!is.na(results$data[,3]),][,c(1,4,3)]
+        names(locs) <- c('spname', 'lon', 'lat')
         values$df <- locs
         c(nrow(locs), results$meta$count)
       })
@@ -43,18 +51,28 @@ shinyServer(function(input, output, session) {
     paste('Total records for', name, 'returned [', out[1], '] out of [', out[2], '] total (limit 500).')
   })
   
-  output$GBIFmap <- renderPlot({
+  plotMap <- function(pts=NULL, poly=NULL) {
     mapWorld <- borders('world', colour = 'white', fill = 'white')
     mp <- ggplot() + mapWorld + 
       theme(panel.background = element_rect(fill = 'lightblue'))
-    if (input$goMap == 0) return(print(mp))
-    input$goMap
-    isolate({
-      xl <- c(min(values$df$lon) - 5, max(values$df$lon) + 5)
-      yl <- c(min(values$df$lat) - 5, max(values$df$lat) + 5)
-      mp <- mp + geom_point(data = values$df, mapping=aes(x = lon, y = lat), color = 'blue', size = 3) +
+    if (!is.null(pts)) {
+      xl <- c(min(pts$lon) - 5, max(pts$lon) + 5)
+      yl <- c(min(pts$lat) - 5, max(pts$lat) + 5)
+      mp <- mp + geom_point(aes(x = lon, y = lat), pts, color = 'blue', size = 3) +
         coord_cartesian(xlim = xl, ylim = yl)
-      print(mp)
+      if (!is.null(poly)) {
+        mp <- mp + geom_path(aes(x = long, y = lat), fortify(poly), color = 'red')
+      }
+    }
+    print(mp)
+  }
+  
+  output$GBIFmap <- renderPlot({
+    if (input$goMap == 0) return(plotMap())
+    input$goMap
+    makeBackgExt()
+    isolate({
+      plotMap(values$df, values$backgExt)
     })
   })
   
@@ -87,11 +105,35 @@ shinyServer(function(input, output, session) {
     paste('Total records thinned to [', nrow(values$df), '] points.')
   })
   
+  makeBackgExt <- reactive({
+    if (input$backg == "") return()
+    
+    if (input$backg == 'bb') {
+      xmin <- min(values$df$lon) - input$backgBuf
+      xmax <- max(values$df$lon) + input$backgBuf
+      ymin <- min(values$df$lat) - input$backgBuf
+      ymax <- max(values$df$lat) + input$backgBuf
+      bb <- matrix(c(xmin, xmin, xmax, xmax, xmin, ymin, ymax, ymax, ymin, ymin), ncol=2)
+      bb <- SpatialPolygons(list(Polygons(list(Polygon(bb)), 1)))
+      values$backgExt <- bb
+    } else if (input$backg == 'mcp') {
+      xy_mcp <- mcp(values$df[,2:3])
+      xy_mcp <- gBuffer(xy_mcp, width = input$backgBuf)
+      values$backgExt <- xy_mcp
+    }
+  })
+  
+  output$test <- renderPrint(input$backg=='bb')
+  
   runENMeval <- reactive({
     input$goEval
     isolate({
       path <- paste0('worldclim/', input$pred)
       preds <- stack(file.path(path, list.files(path, pattern = "bil$")))
+      preds_backgExt <- crop(preds, values$backgExt)
+      preds_backgExt <- mask(preds, values$backgExt)
+      backg_pts <- randomPoints(preds_backgExt, 10000)
+      
       rms <- seq(input$rms[1], input$rms[2], input$rmsBy)
       progress <- shiny::Progress$new()
       progress$set(message = "Evaluating ENMs...", value = 0)
@@ -100,7 +142,7 @@ shinyServer(function(input, output, session) {
       updateProgress <- function(value = NULL, detail = NULL) {
         progress$inc(amount = 1/n, detail = detail)
       }
-      e <- ENMevaluate(values$df, preds, RMvalues = rms, fc = input$fcs, 
+      e <- ENMevaluate(values$df[,2:3], preds_backgExt, bg.coords = backg_pts, RMvalues = rms, fc = input$fcs, 
                        method = input$method, updateProgress = updateProgress)
       values$evalTbl <- e@results
     })
