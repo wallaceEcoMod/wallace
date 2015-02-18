20a16bdbdf07bcfcf36bdeade2cc4aee3aa6504d
tanuki <jamie.m.kass@gmail.com>
2015-02-18 12:22:07 -0500

    changed language in ui
    Former-commit-id: 1054c45356244c5f4c2c350458240671c7b62a5c

--- ui.R
+++ ui.R
@@ -2,17 +2,21 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  titlePanel("ENM Interactive Evaluation with GBIF"),
-  helpText("Calibrate and evaluate ENMs with GBIF data."),
+  titlePanel("WALLACE"),
+  helpText("Harnessing Digital Biodiversity Data via a GUI interface fueled by R"),
   
   sidebarPanel(
     conditionalPanel("input.conditionedPanels == 1",
+                     span(strong("rgbif"), style = "color:purple; font-size:18pt"), br(),
+                     span(em("Filler"), style = "font-size:10pt"), br(),
+                     a("CRAN homepage", href = "http://cran.r-project.org/web/packages/ENMeval/index.html"),
+                     hr(),
                      "The first step is to download occurrence data from GBIF. After we
                      acquire these points, it helps to examine them on a map plot. Future
                      versions will allow the user to upload their own occurrence points as
                      an alternate option.",
                      hr(),
-                     textInput("gbifName", label = "Enter scientific name of species", value = ''),
+                     textInput("gbifName", label = "Enter scientific name of species (format: genus species)", value = ''),
                      actionButton("goName", "Submit name"),
                      br(),
                      br()
@@ -37,16 +41,16 @@ shinyUI(fluidPage(
                      to upload their own rasters.", br(),
                      a("Worldclim homepage", href = "http://worldclim.org"),
                      hr(),
-                     selectInput("pred", label = "Predictor raster resolution",
+                     selectInput("pred", label = "Grid resolution",
                                  choices = list("Choose resolution" = "", 
                                                 "30 arcsec" = "wc30arcsec", 
                                                 "2.5 arcmin" = "wc2.5arcmin", 
                                                 "5 arcmin" = "wc5arcmin", 
                                                 "10 arcmin" = "wc10arcmin")),
-                     selectInput("backg", label = "Background selection",
+                     selectInput("backg", label = "Study region selection",
                                  choices = list("Choose option" = "", "Bounding box" = 'bb', 
                                                 "Minimum convex polygon" = 'mcp')),
-                     numericInput("backgBuf", label = "Background selection buffer distance", value = 0)
+                     numericInput("backgBuf", label = "Study region buffer distance (degree)", value = 0)
     ),
     conditionalPanel("input.conditionedPanels == 4",
                      span(strong("ENMeval"), style = "color:purple; font-size:18pt"), br(),
@@ -57,14 +61,14 @@ shinyUI(fluidPage(
                      and outputs how well each performed on test data. This step aims to help the
                      user decide on an optimal combination of complexity parameters.",
                      hr(),
-                     checkboxGroupInput("fcs", label = "Select feature classes", 
+                     checkboxGroupInput("fcs", label = "Select feature classes (flexibility of modeled response)", 
                                         choices = list("L" = "L", "LQ" = "LQ", "H" = "H",
-                                                       "LQH" = "LQH", "LQHP" = "LQHP", "LQHPT" = "LQHPT"),
-                                        selected = c("L", "LQ")),
-                     sliderInput("rms", label = "Select regularization multipliers",
+                                                       "LQH" = "LQH", "LQHP" = "LQHP", "LQHPT" = "LQHPT")
+                                        ),
+                     sliderInput("rms", label = "Select regularization multipliers (penalty against complexity)",
                                  min = 0, max = 10, value = c(1, 5)),
-                     numericInput("rmsBy", label = "RM step value", value = 1),
-                     selectInput("method", label = "Evaluation method",
+                     numericInput("rmsBy", label = "RM step value", value = 0),
+                     selectInput("method", label = "Occurrence record partitioning method",
                                  choices = list("jackknife" = "jackknife", 
                                                 "block" = "block",
                                                 "checkerboard1" = "checkerboard1", 
@@ -76,24 +80,24 @@ shinyUI(fluidPage(
   
   mainPanel(
     tabsetPanel(id = "conditionedPanels",
-      tabPanel("1) Download GBIF Occurrence Points / Plot", value = 1,
+      tabPanel("1) Download / Plot / Clean Occurrence Data", value = 1,
                br(),
                conditionalPanel("input.goName", textOutput('GBIFtxt')),
                br(),
                plotOutput('GBIFmap1'),
                tableOutput('gbifOccTbl')
                ),
-      tabPanel("2) Thin occurrences", value = 2,
+      tabPanel("2) Process Occurrence Data", value = 2,
                br(),
                conditionalPanel("input.goThin", textOutput('thinText')),
                plotOutput('GBIFmap2'),
                tableOutput('thinOccTbl')
                ),
-      tabPanel("3) Choose Prediction Rasters", value = 3,
+      tabPanel("3) Choose Environmental Variables", value = 3,
                conditionalPanel("input.pred != ''", textOutput('predExtTxt')),
                plotOutput('GBIFmap3')
                ),
-      tabPanel("4) Evaluate ENMs", value = 4,
+      tabPanel("4) Build & Evaluate Niche / Distribution Models", value = 4,
                conditionalPanel("input.goEval", textOutput('evalTxt')),
                tableOutput('evalTbl'))
     )
