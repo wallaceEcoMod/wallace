4dd22918b9ed9bf06a44e44e2bea477468a9ff76
Bruno Vilela de Moraes e Silva <brunovilelasilva@hotmail.com>
2015-02-18 16:25:38 +0100

    ENMeval in the list of packages
    Former-commit-id: 2381f040557cb5de4afd059182347499598a3c7e

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("shiny", "shinyIncubator", "ggplot2", "maps", "rgbif", "spThin")
+list.of.packages <- c("shiny", "shinyIncubator", "ggplot2", "maps", "rgbif", "spThin", "ENMeval")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
