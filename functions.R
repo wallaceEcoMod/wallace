b21042dc0ba3bb72ec7ab5bc7d5769d7d83952ec
ndimhypervol <jamie.m.kass@gmail.com>
2015-06-09 00:11:50 -0400

    broken user input
    Former-commit-id: 768b8e29b67d1862e3d3a334e9877b00392599a9

--- functions.R
+++ functions.R
@@ -36,52 +36,11 @@ makeOccIcons <- function(width = 10, height = 10, ...) {
   files
 }
 
-# this is currently only used for the raster mapping, but was originally used for all maps
-# before we implemented leaflet (the version of leaflet we used did not have raster plotting
-# functionality)-- we plan to recode using the new leaflet version (with new syntax) as a next 
-# step, whereupon a leaflet map will be used for plotting rasters too
-plotMap <- function(pts=NULL, poly=NULL, pred=NULL, addpo=NULL, pts2=NULL) {
-  mapWorld <- borders('world', colour = 'white', fill = 'white')
-  if (!is.null(pts)) {
-    xl <- c(min(pts$lon) - 5, max(pts$lon) + 5)
-    yl <- c(min(pts$lat) - 5, max(pts$lat) + 5)
-    mp <- ggplot(pts, aes(x = lon, y = lat)) + 
-      mapWorld + theme(panel.background = element_rect(fill = 'lightblue')) +
-      geom_point(size = 3, colour = 'blue', position = 'jitter', shape = 1) +
-      coord_cartesian(xlim = xl, ylim = yl) + 
-      geom_text(label = row.names(pts), hjust = 1, vjust = -1)
-    if (!is.null(poly)) {
-      mp <- mp + geom_path(aes(x = long, y = lat), fortify(poly), colour = 'red')
-    }
-    print(mp)
-  }
-  if (!is.null(pred)) {
-    # Convert raster to points
-    pred.p <- rasterToPoints(pred)
-    # Make the points a dataframe for ggplot
-    pred.df <- data.frame(pred.p)
-    #Make appropriate column headings
-    colnames(pred.df) <- c("lon", "lat", "val")
-    e <- extent(pred)
-    if(!addpo){
-      mp <- ggplot(data = pred.df, aes(x = lon, y = lat)) + 
-        mapWorld + theme(panel.background = element_rect(fill = 'lightblue')) + 
-        geom_raster(aes(fill = val)) + 
-        coord_equal() +
-        coord_cartesian(xlim =c(e[1] - 2, e[2] + 2), ylim =c(e[3] - 2, e[4] + 2)) +
-        #scale_fill_gradient("relative suitability (raw output)", limits = c(pred@data@min, pred@data@max), low = 'grey', high = 'blue')
-        scale_fill_gradientn("relative suitability (raw output)", limits = c(pred@data@min, pred@data@max), colours=matlab.like2(50))
-    }else{
-      mp <- ggplot(data = pred.df, aes(x = lon, y = lat)) + 
-        mapWorld + theme(panel.background = element_rect(fill = 'lightblue')) + 
-        geom_raster(aes(fill = val)) + 
-        coord_equal() +
-        coord_cartesian(xlim =c(e[1] - 2, e[2] + 2), ylim =c(e[3] - 2, e[4] + 2)) +
-        #scale_fill_gradient("relative suitability (raw output)", limits = c(pred@data@min, pred@data@max), low = 'grey', high = 'blue')
-        scale_fill_gradientn("relative suitability (raw output)", limits = c(pred@data@min, pred@data@max), colours=matlab.like2(50))
-      mp <- mp + geom_point(data=pts2, aes(x = lon, y = lat), size = 3, colour = 'red',
-                            position = 'jitter', shape = 1)  
-    }
-    print(mp)
-  }
+popUpContent <- function(x) {
+  as.character(tagList(
+    tags$strong(paste("ID:", x['row'])),
+    tags$br(),
+    tags$strong(paste("Latitude:", x['lat'])),        
+    tags$strong(paste("Longitude:", x['lon']))
+  ))
 }
