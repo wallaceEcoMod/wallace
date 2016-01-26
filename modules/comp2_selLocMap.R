c920378a0bec67cf4bd0b9b761db4007d9f10fef
ndimhypervol <jamie.m.kass@gmail.com>
2016-01-26 06:54:32 -0500

    separated funcs for mapping comps 2 & 3, but remove poly doesnt work if you dont select with it

--- modules/comp2_selLocMap.R
+++ modules/comp2_selLocMap.R
@@ -20,25 +20,27 @@ comp2_selLocMap_remLocs <- function(remLoc) {
 }
 
 comp2_selLocMap_selIntLocs <- function() {
+  if (is.null(values$gbifoccs)) return()
   values$prethinned <- NULL  # resets prethinned to avoid it plotting if sequence is: select pts -> spThin -> select pts -> spThin
   values$polyErase <- TRUE  # turn on to signal to prevent the use of an existing map click
   values$polyID <- values$polyID + 1
-  if (is.null(values$gbifoccs)) return()
-  if (is.null(values$drawPolyCoords)) return()
-  pts <- SpatialPoints(values$gbifoccs[,2:3])
-  newPoly <- SpatialPolygons(list(Polygons(list(Polygon(values$drawPolyCoords)), ID=values$polyID)))
-  if (is.null(values$drawPolys)) {
+  if (is.null(values$drawPolyCoordsSelLocs)) return()
+  pts <- SpatialPoints(values$df[,2:3])  # make pts spatial object
+  
+  newPoly <- SpatialPolygons(list(Polygons(list(Polygon(values$drawPolyCoordsSelLocs)), ID=values$polyID)))  # create new polygon from coords
+  if (is.null(values$drawPolys)) {  # if there are no polygons, draw the new one, otherwise draw the new plus all the old ones
     values$drawPolys <- newPoly
   } else {
     values$drawPolys <- spRbind(values$drawPolys, newPoly)
   }
-  values$ptSeln <- as.numeric(which(!(is.na(over(pts, values$drawPolys)))))  # selected locs with polygon
+  
+  values$ptSeln <- as.numeric(which(!(is.na(over(pts, values$drawPolys)))))  # select pts overlapping (intersecting) with polygon(s)
   
   # Subset with selected locs
   ptsSel <- values$gbifoccs[values$ptSeln, ]
   values$df <- ptsSel
   
-  values$drawPolyCoords <- NULL
+  values$drawPolyCoordsSelLocs <- NULL
   values$ptsSel <- ptsSel
   isolate(writeLog(paste('* Selected', nrow(values$df), 'localities.')))
 }
