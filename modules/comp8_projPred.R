4086916403b77f2a97f3ec26d4b727493a49e04d
ndimhypervol <jamie.m.kass@gmail.com>
2016-02-01 02:13:51 -0500

    comp8 map funcs condensed too, but weird crash if actionButton remains in place, moved it down and now no crashes...

--- modules/comp8_projPred.R
+++ modules/comp8_projPred.R
@@ -1,27 +1,29 @@
 comp8_selProjExt <- function() {
   if (is.null(values$df)) return()
-  if (is.null(values$drawPolyCoordsProjExt)) return()
-  values$drawPolyCoordsProjExt <- unique(values$drawPolyCoordsProjExt)  # remove phantom first row after reset
-  if (nrow(values$drawPolyCoordsProjExt) < 3) {
+  if (is.null(values$polyPts2)) return()
+  values$polyPts2 <- unique(values$polyPts2)  # remove phantom first row after reset
+  if (nrow(values$polyPts2) < 3) {
     writeLog("! Please define a shape with at least 3 sides.")
     return()
   }
   values$polyErase <- TRUE  # turn on to signal to prevent the use of an existing map click
   values$polyID <- values$polyID + 1
   
-  values$projExtPoly <- SpatialPolygons(list(Polygons(list(Polygon(values$drawPolyCoordsProjExt)), ID=values$polyID)))  # create new polygon from coords
+  values$poly2 <- SpatialPolygons(list(Polygons(list(Polygon(values$polyPts2)), ID=values$polyID)))  # create new polygon from coords
+  proxy %>% addPolygons(values$polyPts2[,1], values$polyPts2[,2], weight=3, color='red', layerId='poly2Sel')
   
-  x <- round(values$drawPolyCoordsProjExt, digits = 2)  # round all coords to 2 decimal digits
+  
+  x <- round(values$polyPts2, digits = 2)  # round all coords to 2 decimal digits
   coordsChar <- paste(apply(x, 1, function(b) paste0('(',paste(b, collapse=', '),')')), collapse=', ')  # concatanate coords to a single character
-  values$drawPolyCoordsProjExt <- NULL
+  values$polyPts2 <- NULL
   isolate(writeLog(paste0('* Defined projection extent to: ', coordsChar)))
 }
 
 comp8_pjModel <- function(modelSel, preds) {
-  if (is.null(values$projExtPoly)) return()
+  if (is.null(values$poly2)) return()
   curMod <- values$evalMods[[as.numeric(modelSel)]]
-  newMskPreds <- crop(preds, values$projExtPoly)
-  newMskPreds <- mask(newMskPreds, values$projExtPoly)
+  newMskPreds <- crop(preds, values$poly2)
+  newMskPreds <- mask(newMskPreds, values$poly2)
   values$pj <- predict(curMod, newMskPreds)
   rasVals <- getValues(values$pj)
   pal <- colorNumeric(c("#fff5f0", "#fb6a4a", "#67000d"), rasVals, na.color='transparent')
