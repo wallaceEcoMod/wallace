2f3d917856f4f90007ef5766be35e2e135024fea
ndimhypervol <jamie.m.kass@gmail.com>
2016-02-01 23:07:26 -0500

    erased mapFuncs

--- functions.R
+++ functions.R
@@ -2,6 +2,17 @@
 ## Define functions
 ## -------------------------------------------------------------------- ##
 
+# clear current mapped features and plot occurrence locations
+map_plotLocs <- function(locs, clearMarkers=TRUE, clearShapes=TRUE, clearImages=TRUE, fillColor='red', fillOpacity=0.2) {
+  if (clearMarkers) proxy %>% clearMarkers()
+  if (clearShapes) proxy %>% clearShapes()
+  if (clearImages) proxy %>% clearImages()
+  proxy %>% addCircleMarkers(data = locs, lat = ~latitude, lng = ~longitude,
+                             radius = 5, color = 'red', fill = TRUE, 
+                             fillColor = fillColor, fillOpacity = fillOpacity, 
+                             weight = 2, popup = ~pop)
+}
+
 # zooms appropriately for any extent
 smartZoom <- function(longi, lati) {
   lg.diff <- abs(max(longi) - min(longi))
