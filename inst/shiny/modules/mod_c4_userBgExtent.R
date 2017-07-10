623d914a26009b3f9afbc2f668941c9ac37fbc39
ndimhypervol <jamie.m.kass@gmail.com>
2017-07-10 10:43:54 -0400

    user bg shapefile input works

--- inst/shiny/modules/mod_c4_userBgExtent.R
+++ inst/shiny/modules/mod_c4_userBgExtent.R
@@ -3,7 +3,7 @@ userBgExtent_UI <- function(id) {
   ns <- NS(id)
   tagList(
     fileInput(ns("userBgShp"), label = 'Upload polygon with field order: longitude, latitude (.csv)',
-              accept=c(".csv")),
+              accept=c(".csv", ".dbf", ".shx", ".shp"), multiple = TRUE),
     numericInput(ns("userBgBuf"), label = "Study region buffer distance (degree)", value = 0, min = 0, step = 0.5),
     shinyBS::bsPopover('userBgBuf', title = 'Tip',
                        'Buffer area in degrees (1 degree = ~111 km). Exact length varies based on latitudinal position.',
@@ -12,11 +12,7 @@ userBgExtent_UI <- function(id) {
 }
 
 userBgExtent_MOD <- function(input, output, session, logs, occs) {
-  reactive({
-    
-    #       file <- shinyFileChoose(input, 'userBgShp', root=c(root='.'))
-    #       path <- input$userBgShp$datapath
-    print(input$userBgShp)
+  userBgShp <- reactive({
     names <- input$userBgShp$name
     inPath <- input$userBgShp$datapath
     pathdir <- dirname(inPath)
@@ -24,45 +20,34 @@ userBgExtent_MOD <- function(input, output, session, logs, occs) {
     # get extensions of all input files
     exts <- sapply(strsplit(names, '\\.'), FUN=function(x) x[2])
     
-    if (exts == 'csv') {
+    if (length(exts) == 1 & exts == 'csv') {
       f <- read.csv(inPath, header = TRUE)
       
       bgExt <- sp::SpatialPolygons(list(sp::Polygons(list(sp::Polygon(f)), 1)))
+    } else if ('shp' %in% exts) {
+      file.rename(inPath, file.path(pathdir, names))
+      # get index of .shp
+      i <- which(exts == 'shp')
+      # read in shapefile and extract coords
+      bgExt <- readOGR(pathdir[i], strsplit(names[i], '\\.')[[1]][1])
     } else {
-      logs %>% writeLog(type = 'warning', 'Please enter a CSV file of vertex coordinates.')
+      logs %>% writeLog(type = 'warning', 'Please enter either a CSV file of vertex coordinates or shapefile.')
       return()
     }
-    
-    #       } else if (exts == 'shp') {
-    #         # rename temp files to their original names - nice hack for inputting shapefiles in shiny
-    #         sinkRmdob(buf, "Define the buffer size of the study extent:")
-    #
-    #         sinkRmdmult(c(
-    #           file.rename(inPath, file.path(pathdir, names)),
-    #           # get index of .shp
-    #           i <- which(exts == 'shp'),
-    #           # read in shapefile and extract coords
-    #           # poly <- readOGR(pathdir[i], strsplit(names[i], '\\.')[[1]][1])),
-    #           poly <- readShapePoly(file.path(pathdir[i], strsplit(names[i], '\\.')[[1]][1]))),
-    #           "Read the shapefile for the study extent:")
-    #
-    #
-    #         sinkRmdmult(c(
-    #           poly <- rgeos::gBuffer(poly, width = buf + raster::res(values$preds)[1]),
-    #           values$backgExt <- poly,
-    #           bb <- poly@polygons[[1]]@Polygons[[1]]@coords),
-    #           "Generate the user-defined study extent plus the buffer:")
-    #       }
-    
-    bufWid <- input$userBgBuf
-    print(bufWid)
-    if (bufWid > 0) {
-      bgExt <- rgeos::gBuffer(bgExt, width = bufWid)
-    }
-    
     logs %>% writeLog("Study extent: user-defined polygon.")
-    logs %>% writeLog('Study extent buffered by', bufWid, 'degrees.')
-    
     return(bgExt)
   })
+  
+  bufBg <- reactive({
+    bufWid <- input$userBgBuf
+    if (bufWid > 0) {
+      bgExt <- rgeos::gBuffer(userBgShp(), width = bufWid)
+      logs %>% writeLog('Study extent buffered by', bufWid, 'degrees.')
+    } else {
+      bgExt <- userBgShp()
+    }
+    return(bgExt)
+  })
+  
+  return(bufBg)
 }
