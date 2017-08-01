0db51591017be268150bfbcd627e634d49043490
ndimhypervol <jamie.m.kass@gmail.com>
2017-08-01 14:10:22 -0400

    adding selenium testing

---
+++ test/run_wallace.R
@@ -0,0 +1,5 @@
+# Run Wallace
+# Open a different R session and run this code before testing
+require(wallace)
+app_path <- system.file("shiny", package = "wallace")
+return(shiny::runApp(app_path, launch.browser = TRUE, port = 6268, test.mode = TRUE))
