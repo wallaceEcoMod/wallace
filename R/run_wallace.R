67212263e1b560e5114e6ad1a086525ec84f1f4e
ndimhypervol <jamie.m.kass@gmail.com>
2017-11-09 13:03:39 -0500

    oops

---
+++ R/run_wallace.R
@@ -0,0 +1,17 @@
+#'Run \emph{Wallace} Application
+#'
+#' @author Jamie M. Kass
+#'
+#' @description This function runs the \emph{Wallace} application in the user's default web browser.
+#'
+#' @note Please see the official website (\url{https://wallaceecomod.github.io/}) for more details. If you have questions about the application, please participate in the \href{https://groups.google.com/forum/#!forum/wallaceecomod}{Google Group}, or email the team directly: \url{wallaceEcoMod@@gmail.com}.
+#'
+#' @examples \dontrun{
+#' run_wallace()
+#' }
+#'
+#' @export
+run_wallace <- function(){
+  app_path <- system.file("shiny", package = "wallace")
+  return(shiny::runApp(app_path, launch.browser = TRUE))
+}
---
+++ R/wallace-package.R
@@ -0,0 +1,39 @@
+#'\emph{Wallace} is a \code{shiny} app that guides users through a complete species niche/distributional modeling analysis, from the acquisition of species occurrence and environmental data to visualizing model predictions on an interactive map (\code{rleaflet}), thus bundling complex workflows into a single, streamlined GUI interface. New functionality, in the form of modules, can be added to \emph{Wallace} via contributions from the user community. In addition, executable session code (R Markdown format) can be downloaded to share with others or use as supplementary information for scientific papers and reports. The application is run via the function \code{\link{run_wallace}}.
+#'
+#'Please see the official website (\url{https://wallaceecomod.github.io/}) for more details. If you have questions about the application, please participate in the \href{https://groups.google.com/forum/#!forum/wallaceecomod}{Google Group}, or email the team directly: \url{wallaceEcoMod@@gmail.com}.
+#'
+#' @name wallace-package
+#' @aliases wallace
+#' @aliases wallace-package
+#' @docType package
+#' @title \emph{Wallace}: A modular platform for reproducible ecological modeling
+#' @author \strong{Jamie M. Kass}\cr
+#' (email: \email{jkass@@gradcenter.cuny.edu};
+#' Website: \url{https://ndimhypervol.github.io/})
+#' @author \strong{Bruno Vilela}\cr
+#' (email: \email{bvilela@@wustl.edu};
+#' Website: \url{https://bvilela.weebly.com/})
+#' @author \strong{Matthew E. Aeillo-Lammens}\cr
+#' (email: \email{matt.lammens@@gmail.com};
+#' Website: \url{https://mlammens.github.io/})
+#' @author \strong{Robert Muscarella}\cr
+#' (email: \email{bob.muscarella@@gmail.com};
+#' Website: \url{https://bobmuscarella.weebly.com/})
+#' @author \strong{Robert P. Anderson}\cr
+#' (email: \email{randerson@@ccny.cuny.edu};
+#' Website: \url{https://www.andersonlab.ccny.cuny.edu/})
+#'
+#' @keywords package
+#'
+#' @details \tabular{ll}{
+#' Package: \tab wallace\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.6.4\cr
+#' Date: \tab 2017-05-17\cr
+#' License: \tab GNU 3.0\cr
+#' }
+#'
+#' @references Kass J.M., Vilela B., Aeillo-Lammens M.E., Muscarella R., and Anderson R.P. (2017) \emph{Wallace}: A modular platform for reproducible ecological modeling. Version 0.6.4.
+#' @import shiny leaflet
+#' @importFrom magrittr "%>%"
+NULL
