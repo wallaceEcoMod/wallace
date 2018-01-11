b7b0a0c0b4d25bdadabb6d490f7eada9e1680888
obroenni <obroenni@gmail.com>
2018-01-11 10:29:00 -0500

    PCA 1 or 2 backgrounds

--- R/cESpace_pca.R
+++ R/cESpace_pca.R
@@ -24,12 +24,20 @@ cESpace_PCA<- function(bgMask1, bgMask2=NULL, occs1, occs2=NULL, logs=NULL, shin
   library(raster)
   
   df1<-na.exclude(getValues(bgMask1))
-  df2<-na.exclude(getValues(bgMask2))
+  if (!is.null(bgMask2)) df2<-na.exclude(getValues(bgMask2))
   
-  data<-rbind(occs1[,-(1:2)],occs2[,-(1:2)],df1,df2)
-  
-  sp<-c(rep(1,nrow(occs1)),rep(2,nrow(occs2)),rep(0,nrow(df1)),rep(0,nrow(df2)))
-  bg<-c(rep(0,nrow(occs1)),rep(0,nrow(occs2)),rep(1,nrow(df1)),rep(2,nrow(df2)))
+  if (!is.null(bgMask2)){
+    
+    data<-rbind(occs1[,-(1:2)],occs2[,-(1:2)],df1,df2)
+    sp<-c(rep(1,nrow(occs1)),rep(2,nrow(occs2)),rep(0,nrow(df1)),rep(0,nrow(df2)))
+    bg<-c(rep(0,nrow(occs1)),rep(0,nrow(occs2)),rep(1,nrow(df1)),rep(2,nrow(df2)))
+    
+  } else{
+    
+    data<-rbind(occs1[,-(1:2)],df1)
+    sp<-c(rep(1,nrow(occs1)),rep(0,nrow(df1)))
+    bg<-c(rep(0,nrow(occs1)),rep(1,nrow(df1)))
+  }
   
   #pca calibration and predicition of scores
   
@@ -37,8 +45,9 @@ cESpace_PCA<- function(bgMask1, bgMask2=NULL, occs1, occs2=NULL, logs=NULL, shin
   
   scores<-cbind(pca$li,sp,bg)
   
-  s.class(scores[sp==0,1:2],as.factor(scores[sp==0,]$bg),col=c("blue","red"),cstar = 0, cpoint=0.1)
-  s.corcircle(pca$co, lab = names(pca$tab), full = FALSE, box = TRUE)
+  # plots
+  #s.class(scores[sp==0,1:2],as.factor(scores[sp==0,]$bg),col=c("blue","red"),cstar = 0, cpoint=0.1)
+  #s.corcircle(pca$co, lab = names(pca$tab), full = FALSE, box = TRUE)
   
   pca<-list(scores,pca$eig,pca$co)
   
