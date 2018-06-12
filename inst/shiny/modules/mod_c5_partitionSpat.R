2470e18e58f2da1ceefb57a2af122a6ae39cdbf9
ndimhypervol <jamie.m.kass@gmail.com>
2018-06-12 15:16:29 -0400

    fixed sp partition mod

--- inst/shiny/modules/mod_c5_partitionSpat.R
+++ inst/shiny/modules/mod_c5_partitionSpat.R
@@ -13,38 +13,40 @@ partSp_UI <- function(id) {
 
 partSp_MOD <- function(input, output, session) {
   reactive({
-    if (is.null(bgMask())) {
-      shinyLogs %>% writeLog(type = 'error', "Before partitioning occurrences for ", spName(spp[[sp]]),
-                       ", mask your environmental variables by your background extent.")
-      return()
-    }
-    
-    # FUNCTION CALL ####
-    group.data <- c5_partitionOccs(occs(), 
-                                   bg(), 
-                                   input$partSpSel, 
-                                   bgMsk = bgMask(), 
-                                   aggFact = input$aggFact, 
-                                   shinyLogs)
-    req(group.data)
-    
-    # LOAD INTO SPP ####
-    spp[[curSp()]]$occs$partition <- group.data$occ.grp
-    spp[[curSp()]]$bg$partition <- group.data$bg.grp
+    for(sp in spIn()) {
+      if (is.null(bgMask())) {
+        shinyLogs %>% writeLog(type = 'error', "Before partitioning occurrences for ", sp,
+                               ", mask your environmental variables by your background extent.")
+        return()
+      }
       
-    # METADATA ####
-    if(input$partSpSel == 'block') {
-      spp[[curSp()]]$rmm$model$partition$numberFolds <- 4
-      spp[[curSp()]]$rmm$model$partition$partitionRule <- 'spatial block'
-    }
-    if(input$partSpSel == 'cb1') {
-      spp[[curSp()]]$rmm$model$partition$numberFolds <- 2
-      spp[[curSp()]]$rmm$model$partition$partitionRule <- 'checkerboard'
-    }
-    if(input$partSpSel == 'cb2') {
-      spp[[curSp()]]$rmm$model$partition$numberFolds <- 4
-      spp[[curSp()]]$rmm$model$partition$partitionRule <- 'hierarchical checkerboard'
-      spp[[curSp()]]$rmm$model$partition$notes <- paste('aggregation factor =', input$aggFact)
+      # FUNCTION CALL ####
+      group.data <- c5_partitionOccs(occs(), 
+                                     bg(), 
+                                     input$partSpSel, 
+                                     bgMsk = bgMask(), 
+                                     aggFact = input$aggFact, 
+                                     shinyLogs)
+      req(group.data)
+      
+      # LOAD INTO SPP ####
+      spp[[sp]]$occs$partition <- group.data$occ.grp
+      spp[[sp]]$bg$partition <- group.data$bg.grp
+      
+      # METADATA ####
+      if(input$partSpSel == 'block') {
+        spp[[sp]]$rmm$model$partition$numberFolds <- 4
+        spp[[sp]]$rmm$model$partition$partitionRule <- 'spatial block'
+      }
+      if(input$partSpSel == 'cb1') {
+        spp[[sp]]$rmm$model$partition$numberFolds <- 2
+        spp[[sp]]$rmm$model$partition$partitionRule <- 'checkerboard'
+      }
+      if(input$partSpSel == 'cb2') {
+        spp[[sp]]$rmm$model$partition$numberFolds <- 4
+        spp[[sp]]$rmm$model$partition$partitionRule <- 'hierarchical checkerboard'
+        spp[[sp]]$rmm$model$partition$notes <- paste('aggregation factor =', input$aggFact)
+      }
     }
     
   })
