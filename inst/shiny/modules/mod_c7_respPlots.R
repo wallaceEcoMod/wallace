b58b09673d2803d005ffd3874b848cab64890a7d
Gonzalo <gepinillab@gmail.com>
2018-09-06 10:47:19 -0400

    responsePlots working for maxent.jar

--- inst/shiny/modules/mod_c7_respPlots.R
+++ inst/shiny/modules/mod_c7_respPlots.R
@@ -20,20 +20,36 @@ respPlots_MOD <- function(input, output, session, rvs) {
     
     modCur <- rvs$mods[[rvs$modSel]]
     
-    # handle downloads for Response Plots png
-    output$dlRespPlot <- downloadHandler(
-      filename = function() {paste0(spName(), "_", rvs$envSel, "_response.png")},
-      content = function(file) {
-        png(file)
-        if (input$dlRespPlotAll == TRUE) {
-          plot(modCur, type = "cloglog")
-        } else {
-          maxnet::response.plot(modCur, v = rvs$envSel, type = "cloglog")  
+    # handle downloads for Response Plots png for Maxnet
+    if (rvs$algMaxent == "maxnet") {
+      output$dlRespPlot <- downloadHandler(
+        filename = function() {paste0(spName(), "_", rvs$envSel, "_response.png")},
+        content = function(file) {
+          png(file)
+          if (input$dlRespPlotAll == TRUE) {
+            plot(modCur, type = "cloglog")
+          } else {
+            maxnet::response.plot(modCur, v = rvs$envSel, type = "cloglog")  
+          }
+          dev.off()
         }
-        dev.off()
-      }
-    )
-    
-    maxnet::response.plot(modCur, v = rvs$envSel, type = "cloglog")
+      )
+      maxnet::response.plot(modCur, v = rvs$envSel, type = "cloglog")
+    } else if (rvs$algMaxent == "maxent.jar") {
+      # handle downloads for Response Plots png for maxent.jar
+      output$dlRespPlot <- downloadHandler(
+        filename = function() {paste0(spName(), "_", rvs$envSel, "_response.png")},
+        content = function(file) {
+          png(file)
+          if (input$dlRespPlotAll == TRUE) {
+            dismo::response(modCur)
+          } else {
+            dismo::response(modCur, var = rvs$envSel)  
+          }
+          dev.off()
+        }
+      )
+      dismo::response(modCur, var = rvs$envSel)
+    }
   })
 }
