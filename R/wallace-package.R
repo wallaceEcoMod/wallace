b6f9aa7209c86e8d65afa7958f2f33f6c7b8c32a
Gonzalo <gepinillab@gmail.com>
2018-10-05 14:03:25 -0400

    change info to v1.0.6

--- R/wallace-package.R
+++ R/wallace-package.R
@@ -33,12 +33,12 @@
 #' @details \tabular{ll}{
 #' Package: \tab wallace\cr
 #' Type: \tab Package\cr
-#' Version: \tab 1.0.5.9*\cr
-#' Date: \tab 2018-10-08\cr
+#' Version: \tab 1.0.6\cr
+#' Date: \tab 2018-10-05\cr
 #' License: \tab GNU 3.0\cr
 #' }
 #'
-#' @references Kass J.M., Pinilla-Buitrago, G.E., Vilela B., Aeillo-Lammens M.E., Muscarella R., Merow C., and Anderson R.P. (2018) \emph{Wallace}: A modular platform for reproducible ecological modeling. Version 1.0.5.9*
+#' @references Kass J.M., Pinilla-Buitrago, G.E., Vilela B., Aeillo-Lammens M.E., Muscarella R., Merow C., and Anderson R.P. (2018) \emph{Wallace}: A modular platform for reproducible ecological modeling. Version 1.0.6
 #' @import shiny leaflet
 #' @importFrom magrittr "%>%"
 NULL
