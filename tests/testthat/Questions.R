fcc3177c04a8593ea91c61b630f6001a80b74ecd
valengsb <valengsb@gmail.com>
2018-12-18 14:47:24 -0500

    Questions

---
+++ tests/testthat/Questions.R
@@ -0,0 +1,15 @@
+################################################################################
+### QUESTIONS ##################################################################
+
+# 1. Am I following the Wallace's style? >> Yes
+
+# 2. Are my comments (#) on the code enlightening enough? >> Yes
+
+# 3. Am I missing something important or if on the contrary, 
+# the test is being redundant? >> No, I'm not 
+
+# 5. is it ok as I'm managing the test data? those folders >> Yes, it is OK
+
+# 6. Libraries at the beginig (after the context, or should I use Library::function
+
+# 7. Shiny maessages test?
