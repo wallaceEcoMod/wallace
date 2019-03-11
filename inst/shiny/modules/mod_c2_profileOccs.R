a76c1caa332b5813b3c77b22a1421f9e61285181
Cory Merow <cory.merow@gmail.com>
2019-03-11 16:21:19 -0400

    attempt to filter profiled occurrences; can't access input$grades and jamie will work some magic.

--- inst/shiny/modules/mod_c2_profileOccs.R
+++ inst/shiny/modules/mod_c2_profileOccs.R
@@ -2,17 +2,20 @@
 profileOccs_UI <- function(id) {
   ns <- NS(id)
   tagList(
+    # CM: what are we doing with this?
     # tags$div(checkboxInput(ns("spThinAllSp"), label = "Batch for all species?", value = TRUE),
     tags$div(
       title='Create a profile characterizing occurrence records for cleaning and filtering.',
-      checkboxGroupInput(ns("grades"), label = strong("Choose grades to keep"), choices = list("A: xxx" = "A",
-                                                                                               "B: xxx" = "B",
-                                                                                               "C: xxx" = "C",
-                                                                                               "D: xxx" = "D",
-                                                                                               "E: xxx" = "E",
-                                                                                               "F: xxx" = "F",
-                                                                                               "G: xxx" = "G",
-                                                                                               "H: xxx" = "H")),
+      checkboxGroupInput(ns("grades"), 
+                         label = strong("Choose grades to keep"), 
+                         choices = list("A: xxx" = "A",
+                                        "B: xxx" = "B",
+                                        "C: xxx" = "C",
+                                        "D: xxx" = "D",
+                                        "E: xxx" = "E",
+                                        "F: xxx" = "F",
+                                        "G: xxx" = "G",
+                                        "H: xxx" = "H")),
       checkboxInput(ns("batch"), label = strong("Batch"), value = FALSE)
     )
   )
@@ -59,10 +62,12 @@ profileOccs_MOD <- function(input, output, session) {
                                                                 '#abd9e9','#e0f3f8',
                                                                 '#fee090','#fdae61',
                                                                 '#f46d43','#d73027'))
-          color.to.plot<-as.character(sapply(full.qaqc$quality.grade, function (x) proposed.color.grading[x,'color.qgrade']))
+          color.to.plot<-as.character(sapply(full.qaqc$quality.grade, 
+                                             function (x) proposed.color.grading[x,'color.qgrade']))
           
           full.qaqc$grade.color <- color.to.plot
-          ggplot2::ggplot(ggplot2::aes(x=quality.grade,fill=quality.grade), data = full.qaqc) + ggplot2::geom_bar() 
+          ggplot2::ggplot(ggplot2::aes(x=quality.grade,fill=quality.grade), data = full.qaqc) + 
+            ggplot2::geom_bar() 
         })
         
         tabsetPanel(
@@ -84,8 +89,57 @@ profileOccs_MOD <- function(input, output, session) {
   })
 }
 
-# profileOccsClean_MOD <- function(input, output, session) {
-# }
+profileOccsClean_MOD <- function(input, output, session) {
+  reactive({
+    # loop over all species if batch is on
+    #if(input$batch == TRUE) spLoop <- allSp() else spLoop <- curSp()
+    spLoop <- curSp()
+    for(sp in spLoop) {
+      # FUNCTION CALL ####
+      #########
+      # make sure some have been selected
+      # CM: how do I put a shinylog here?
+      # if (is.null(input$grades)) {
+      #   shinyLogs %>% writeLog(type = 'error', 
+      #                          'You must select some grades to keep.')
+      #   return()
+      # }
+      occs=occs()
+      # CM: i think occs should have all the grades with it
+      # check this uses the right formats
+      print(class(input$grades))
+      print('       ')
+      print(input$grades)
+      print('       ')
+      print(unlist(input$grades) )
+      
+      keep= print(occs.thin$occ_short_profile$quality.grade) %in% 
+        unlist(input$grades) 
+      print(keep)
+      # Test that at least some presences pass the specified test
+      if(length(keep)==0) {
+        shinyLogs %>% writeLog(type = 'error',
+                              'You must select some grades to keep.')
+        return()
+      }
+      
+      occsClean=occs[keep,]
+
+      shinyLogs %>% writeLog( 
+        em(spName(occs)), ": Removing dirty occurrences")
+      
+      ########
+      
+      # LOAD INTO SPP ####
+      spp[[sp]]$occs <- occs.clean 
+    
+      # METADATA ####
+      # decide later on metadata
+      # spp[[sp]]$rmm$code$wallaceSettings$thinDistKM <- input$thinDist 
+      # CM: add a vector of which grades/tests kept
+    }
+  })
+}
 
 occProfile_MAP <- function(map, session) {
   updateTabsetPanel(session, 'main', selected = 'Map')
@@ -109,3 +163,4 @@ occProfile_MAP <- function(map, session) {
 profileOccs_INFO <- infoGenerator(modName = "Profile Occurrences",
                                modAuts = "Pep Serra, Cory Merow, Jamie M. Kass",
                                pkgName = "occProfileR")
+
