646164e76d4eb0bf7787a6ce776459bd3ac0e957
Dean Attali <daattali@gmail.com>
2019-08-24 17:42:17 -0400

    bug fix: spIn() doesn't exist

--- inst/shiny/modules/mod_c3_ecoclimate.R
+++ inst/shiny/modules/mod_c3_ecoclimate.R
@@ -6,21 +6,21 @@ ecoClimate_UI <- function(id) {
              selectInput(ns("bcAOGCM"), label = "Select the Atmospheric Oceanic General Circulation Model you want to use",
                          choices = list("Select AOGCMs" = "",
                                         "CCSM" = "CCSM",
-                                        "CNRM"= "CNRM", 
-                                        "MIROC"="MIROC", 
-                                        "FGOALS"="FGOALS", 
-                                        "GISS"="GISS", 
+                                        "CNRM"= "CNRM",
+                                        "MIROC"="MIROC",
+                                        "FGOALS"="FGOALS",
+                                        "GISS"="GISS",
                                         "IPSL"="IPSL",
-                                        "MRI"= "MRI", 
+                                        "MRI"= "MRI",
                                         "MPI"= "MPI"
                          ))),
-    
+
     checkboxInput(ns("ecoClimSelChoice"), label = "Specify variables to use in analysis?"),
     conditionalPanel(paste0("input['", ns("ecoClimSelChoice"), "']"),
                      checkboxGroupInput(ns("ecoClimSel"), label = "Select",
-                                        choices = setNames(as.list(paste0('bio', 1:19)), paste0('bio', 1:19)), 
+                                        choices = setNames(as.list(paste0('bio', 1:19)), paste0('bio', 1:19)),
                                         inline=TRUE, selected = paste0('bio', 1:19)))
-    
+
   )
 }
 
@@ -28,29 +28,29 @@ ecoClimate_MOD <- function(input, output, session, shinyLogs) {
   reactive({
     # ERRORS ####
     if (is.null(vals$occs)) {
-      shinyLogs %>% writeLog(type = 'error', "Before obtaining environmental variables, 
+      shinyLogs %>% writeLog(type = 'error', "Before obtaining environmental variables,
                         obtain occurrence data in component 1.")
       return()
     }
-    
+
     # FUNCTION CALL ####
     timeInterval <- spp[[n]]$rmm$data$occurrence$yearMax
     ecoClims <- c3_ecoClimate(input$bcAOGCM, timeInterval, input$ecoClimSel, shinyLogs)
-    
+
     # PROCESSING ####
-    for(sp in spIn) {
+    for(sp in list(curSp())) {
       # remove occurrences with NA values for variables
       withProgress(message = paste0("Extracting environmental values for occurrences of ", sp, "..."), {
         occsEnvsVals <- as.data.frame(raster::extract(ecoClims, spp[[sp]]$occs[c('longitude', 'latitude')]))
       })
       # remove occurrences with NA environmental values
       spp[[sp]]$occs <- remEnvsValsNA(spp[[sp]]$occs, occsEnvsVals, shinyLogs)
-      
+
       # LOAD INTO SPP ####
       spp[[sp]]$envs <- ecoClims
       # add columns for env variables beginning with "envs_" to occs tbl
       spp[[sp]]$occs <- cbind(spp[[sp]]$occs, occsEnvsVals)
-      
+
       # METADATA ####
       spp[[sp]]$rmm$data$environment$variableNames <- names(ecoClims)
       spp[[sp]]$rmm$data$environment$yearMin <- timeInterval
@@ -63,9 +63,9 @@ ecoClimate_MOD <- function(input, output, session, shinyLogs) {
 }
 
 ecoclimate_MAP <- function(map, session) {
-  map %>% clearAll() %>%     
-    addCircleMarkers(data = occs(), lat = ~latitude, lng = ~longitude, 
-                     radius = 5, color = 'red', fill = TRUE, fillColor = "red", 
+  map %>% clearAll() %>%
+    addCircleMarkers(data = occs(), lat = ~latitude, lng = ~longitude,
+                     radius = 5, color = 'red', fill = TRUE, fillColor = "red",
                      fillOpacity = 0.2, weight = 2, popup = ~pop)
 }
 
