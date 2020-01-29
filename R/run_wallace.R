5ba1ce06134ad6fc8eddc56805db435b31da563d
Gonzalo <gepinillab@gmail.com>
2020-01-29 12:39:38 -0500

    More CRAN changes

--- R/run_wallace.R
+++ R/run_wallace.R
@@ -2,9 +2,16 @@
 #'
 #' @author Jamie M. Kass
 #'
-#' @description This function runs the \emph{Wallace} application in the user's default web browser.
+#' @description This function runs the \emph{Wallace} application in the user's 
+#' default web browser.
 #'
-#' @note Please see the official website (\url{https://wallaceecomod.github.io/}) for more details. If you have questions about the application, please participate in the \href{https://groups.google.com/forum/#!forum/wallaceecomod}{Google Group}, or email the team directly: \url{wallaceEcoMod@@gmail.com}.
+#' @note Please see the official website (\url{https://wallaceecomod.github.io/})
+#'  for more details. If you have questions about the application, please 
+#'  participate in the \href{https://groups.google.com/forum/#!forum/wallaceecomod}{Google Group}, 
+#'  or email the team directly: \email{wallaceecomod@@gmail.com}.
+#' 
+#' @param launch.browser Run Wallace in browser
+#' @param port If launch.browser is FALSE, specify port to run Wallace
 #'
 #' @examples if(interactive()){
 #' run_wallace()
