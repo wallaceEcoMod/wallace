75c8837a0329af42d5b69d6f6134746ad3721eec
Andrea Paz <andreapaz@Andreas-MacBook-Pro-2.local>
2020-03-24 12:08:21 -0500

    Fixed paths Fixed error checks

--- tests/testthat/test_penvs_userBgExtent.R
+++ tests/testthat/test_penvs_userBgExtent.R
@@ -1,6 +1,6 @@
 ##### QUESTIONS
 # 1. error with message: Please enter either a CSV file of vertex coordinates or shapefile (.shp, .shx, .dbf).
-
+## this still needs a test for points outside of polygon part
 
 #### COMPONENT 4: Process Environmental Data
 #### MODULE: user-specified
@@ -10,29 +10,31 @@ source("test_helper_functions.R")
 
 
 ### Set parameters
+## occurrences
+occs <-  occs_queryDb(spName = "panthera onca", occDb = "gbif", occNum = 100)
+occs <- as.data.frame(occs[[1]]$cleaned)
 ## path to files
-Path <- list.files(path='./shapefile/', pattern = "COL_adm0.", full.names = TRUE)
+Path <- list.files(path='./tests/testthat/shapefile', pattern = "COL_adm0.", full.names = TRUE)
 ## files name
-Name <- list.files(path='./shapefile/', pattern = "COL_adm0.", full.names = FALSE)
+Name <- list.files(path='./tests/testthat/shapefile', pattern = "COL_adm0.", full.names = FALSE)
 
 ### generate wrong parameters (to test error messages)
-Name.s <- list.files(path='./shapefile/', pattern = "COL_adm0.s", full.names = FALSE)
-Name.p <- list.files(path='./shapefile/', pattern = "COL_adm0.p", full.names = FALSE)
+Name.s <- list.files(path='./tests/testthat/shapefile', pattern = "COL_adm0.s", full.names = FALSE)
+Name.p <- list.files(path='./tests/testthat/shapefile', pattern = "COL_adm0.p", full.names = FALSE)
 
 
 ### run function
 ## Buffer == 0.5
-userBgbf <- penvs_userBgExtent(bgShp_path = Path, bgShp_name = Name, userBgBuf = 0.5)
+userBgbf <- penvs_userBgExtent(bgShp_path = Path, bgShp_name = Name, userBgBuf = 0.5,occs=occs)
 ## Buffer == 0
-userBg <- penvs_userBgExtent(bgShp_path = Path, bgShp_name = Name, userBgBuf = 0)
+userBg <- penvs_userBgExtent(bgShp_path = Path, bgShp_name = Name, userBgBuf = 0,occs=occs)
 
 
 ### test if the error messages appear when they are supposed to
 test_that("error checks", {
   # the user has not loaded the environmental data
-  expect_error(penvs_userBgExtent(bgShp_path = Path, bgShp_name = Name.s, userBgBuf = 0.5),'If entering a shapefile, please
-                               select all the following files: .shp, .shx, .dbf.')
-  expect_error(penvs_userBgExtent(bgShp_path = Path, bgShp_name = Name.p, userBgBuf = 0.5 ))
+  expect_error(penvs_userBgExtent(bgShp_path = Path, bgShp_name = Name.s, userBgBuf = 0.5,occs=occs),'If entering a shapefile, please select all the following files: .shp, .shx, .dbf.',fixed=T)
+  expect_error(penvs_userBgExtent(bgShp_path = Path, bgShp_name = Name.p, userBgBuf = 0.5, occs=occs ),'Please enter either a CSV file of vertex coordinates or shapefile (.shp, .shx, .dbf).',fixed=T)
 })
 
 ### test output features
