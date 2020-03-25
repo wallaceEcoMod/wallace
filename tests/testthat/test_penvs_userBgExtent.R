0b5f307dfe6e6a6fa273d528323b90b56d30d706
Andrea Paz <andreapaz@Andreas-MacBook-Pro-2.local>
2020-03-25 12:49:36 -0500

    Included new test for error on points outside the user provided background

--- tests/testthat/test_penvs_userBgExtent.R
+++ tests/testthat/test_penvs_userBgExtent.R
@@ -1,13 +1,27 @@
 ##### QUESTIONS
-<<<<<<< HEAD
-# 1. error with message: Please enter either a CSV file of vertex coordinates or shapefile (.shp, .shx, .dbf). From Andres: this is fixed
-## 2-this still needs a test for points outside of polygon part
-##3- There is still a  problem with the function given occs with CRS to fix
-=======
-# 1. error with message: Please enter either a CSV file of vertex coordinates or shapefile (.shp, .shx, .dbf).
-## this still needs a test for points outside of polygon part
->>>>>>> 75c8837a0329af42d5b69d6f6134746ad3721eec
 
+# 1. error with message: Please enter either a CSV file of vertex coordinates or shapefile (.shp, .shx, .dbf).
+        ###From Andres: this is fixed
+
+##3- There is still a  problem with the function given occs with CRS to fix
+## 2-this still needs a test for points outside of polygon part
+
+
+if (ptRem == 0) {
+  if (userBgBuf > 0) {
+    logger %>% writeLog('Study extent user-defined polygon buffered by ',
+                        userBgBuf, ' degrees.')
+  } else {
+    logger %>% writeLog("Study extent: user-defined polygon.")
+  }
+  return(bgExt)
+} else if (ptRem > 0) {
+  logger %>%
+    writeLog(type = 'error',
+             paste0("The polygon did not include all localities(**). ",
+                    "You can remove localities in Process Occs component"))
+  return()
+}
 #### COMPONENT 4: Process Environmental Data
 #### MODULE: user-specified
 context("userBgExtent")
@@ -20,13 +34,14 @@ source("test_helper_functions.R")
 occs <-  occs_queryDb(spName = "panthera onca", occDb = "gbif", occNum = 100)
 occs <- as.data.frame(occs[[1]]$cleaned)
 ## path to files
-Path <- list.files(path='./tests/testthat/shapefile', pattern = "COL_adm0.", full.names = TRUE)
+Path <- list.files(path='./shapefile', pattern = "COL_adm0.", full.names = TRUE)
+
 ## files name
-Name <- list.files(path='./tests/testthat/shapefile', pattern = "COL_adm0.", full.names = FALSE)
+Name <- list.files(path='./shapefile', pattern = "COL_adm0.", full.names = FALSE)
 
 ### generate wrong parameters (to test error messages)
-Name.s <- list.files(path='./tests/testthat/shapefile', pattern = "COL_adm0.s", full.names = FALSE)
-Name.p <- list.files(path='./tests/testthat/shapefile', pattern = "COL_adm0.p", full.names = FALSE)
+Name.s <- list.files(path='./shapefile', pattern = ".s", full.names = FALSE)
+Name.p <- list.files(path='./shapefile', pattern = ".prj", full.names = FALSE)
 
 
 ### run function
@@ -41,7 +56,8 @@ test_that("error checks", {
   # the user has not loaded the environmental data
   expect_error(penvs_userBgExtent(bgShp_path = Path, bgShp_name = Name.s, userBgBuf = 0.5,occs=occs),'If entering a shapefile, please select all the following files: .shp, .shx, .dbf.',fixed=T)
   expect_error(penvs_userBgExtent(bgShp_path = Path, bgShp_name = Name.p, userBgBuf = 0.5, occs=occs ),'Please enter either a CSV file of vertex coordinates or shapefile (.shp, .shx, .dbf).',fixed=T)
-})
+  #The user background does not include all ccurrences
+  })
 
 ### test output features
 test_that("output type checks", {
