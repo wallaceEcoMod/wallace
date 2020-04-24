b09cef1644c598ca5fa5a39b92dada1ea6efa1b6
Gonzalo E. Pinilla-Buitrago <gepinillab@gmail.com>
2020-04-24 12:01:36 -0400

    Bionames working with test and shiny

--- R/envs_worldclim.R
+++ R/envs_worldclim.R
@@ -5,7 +5,7 @@
 #' See Examples.
 #'
 #' @param bcRes numeric resolution of the climatic layers
-#' @param bcSel list of boolean data. selected variables
+#' @param bcSel chacter vector with bionames selected
 #'
 # @keywords
 #'
@@ -30,16 +30,19 @@ envs_worldclim <- function(bcRes, bcSel, mapCntr, doBrick, logger = NULL) {
   }
 
   smartProgress(logger, message = "Retrieving WorldClim data...", {
-    if (bcRes == 0.5) {
       wcbc <- raster::getData(name = "worldclim", var = "bio", res = bcRes,
                               lon = mapCntr[1], lat = mapCntr[2])
-    } else {
-      wcbc <- raster::getData(name = "worldclim", var = "bio", res = bcRes)
-      nam_wcbc <- names(wcbc)
+      # change names if bio01 is bio1, and so forth
+      if (bcRes == 0.5) {
+        names(wcbc) <- gsub("_.*", "", names(wcbc))
+      }
+      i <- grep('bio[0-9]$', names(wcbc))
+      editNames <- paste('bio', sapply(strsplit(names(wcbc)[i], 'bio'), function(x) x[2]), sep = '0')
+      names(wcbc)[i] <- editNames
+
       wcbc <- wcbc[[bcSel]]
-      names(wcbc) <- nam_wcbc
-    }
   })
+
   # convert to brick for faster processing
   if(doBrick == TRUE) {
     smartProgress(logger, message = "Converting to RasterBrick for faster processing...", {
@@ -47,13 +50,6 @@ envs_worldclim <- function(bcRes, bcSel, mapCntr, doBrick, logger = NULL) {
     })
   }
 
-  # change names if bio01 is bio1, and so forth
-  if (bcRes == 0.5) {
-    names(wcbc) <- gsub("_.*", "", names(wcbc))
-  }
-  i <- grep('bio[0-9]$', names(wcbc))
-  editNames <- paste('bio', sapply(strsplit(names(wcbc)[i], 'bio'), function(x) x[2]), sep = '0')
-  names(wcbc)[i] <- editNames
   logger %>% writeLog("WorldClim bioclimatic variables ",
                       paste(names(wcbc), collapse = ", "), " at ",
                       bcRes, " arcmin resolution.")
