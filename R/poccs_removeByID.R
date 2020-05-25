46fa545cfacea05a225a39e3e525be9b65aa11c7
Andrea Paz <andreapaz@Andreas-MacBook-Pro-2.local>
2020-05-25 15:11:27 -0500

    Started function documentation

--- R/poccs_removeByID.R
+++ R/poccs_removeByID.R
@@ -1,14 +1,20 @@
 
 #' @title poccs_removeByID
-#' @description ..
+#' @description This function removes user selected occurrences by ID
 #'
 #' @details
-#' See Examples.
+#' #' @details
+#' This function is called by the select occurrences by ID module. It allows for removal of
+#' occurrences flagged by the user on the map. The function will return
+#' a data frame of occurrences with all relevant columns for futher analyses and without the
+#' occurrences selected by the user
 #'
-#' @param occs x
+#' @param occs data frame of cleaned occurrences obtained from component occs: Obtain occurrence data.
 #' @param removeID x
-#' @param logger x
-#' @param spN x
+#' @param logger stores all notification messages to be displayed in the Log Window of Wallace GUI. insert the logger reactive list here for running in shiny,
+#'  otherwise leave the default NULL
+#' @param spN data frame of cleaned occurrences obtained from component occs: Obtain occurrence data. Used to obtain species name for logger messages
+
 # @keywords
 #'
 # @examples
