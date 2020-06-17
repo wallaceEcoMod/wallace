17cbf69eae355a6d05596625b1348f399c84655e
Andrea Paz <andreapaz@Andreas-MacBook-Pro-2.local>
2020-06-17 16:27:25 -0500

    small documentation updates Updated projection with maxnet to match new ENMeval function and parameters

--- R/proj_area.R
+++ R/proj_area.R
@@ -3,15 +3,16 @@
 #' @description Function projects the model generated in previous components to a new user drawn area
 #'
 #' @details
-#' See Examples.
-#'
+#' This functions allows for the projection of the model created in previous components to a new area.
+#' The projection area is user provided in the map of the GUI. The model will be porjected to the new area as long as the environmental variables are available for the area.
+#' This function returns a list including the cropped environmental variables used for projecting and the projected model.
 #' @param evalOut ENMevaluate output from previous module and using any of the available algorithms
 #' @param curModel If algorithm is maxent, model selected by user as best or optimal, in terms of feature class and regularization multiplier (e.g 'L_1'). Else must be 1
 #' @param envs Environmental layers to be used for projecting the model. They must matchthe layers used for generating the model in the model component
 #' @param outputType Output type to be used when algorithm is maxnet or maxent.jar.
-#' @param alg Modeling algorithm used in the model component. Can be one of : "bioclim", "maxent.jar" or "maxnet"
+#' @param alg Modeling algorithm used in the model component. Can be one of : 'bioclim', 'maxent.jar' or 'maxnet'
 #' @param clamp logical, wether projection will be of clamped or uncalmped model.
-#' @param pjExt Extent of the area to project the model to. This is defined by the user in the map of the GUI.
+#' @param pjExt Extent of the area to project the model to. This is defined by the user in the map of the GUI and is provided as a matrix of latitude, longitude values.
 #' @param logger logger stores all notification messages to be displayed in the Log Window of Wallace GUI. insert the logger reactive list here for running in shiny,
 #'  otherwise leave the default NULL
 # @keywords
@@ -19,12 +20,14 @@
 # @examples
 #'
 #'
-# @return
+#' @return A list of two elements: projExt and projArea.
+#' The first is a RasterBrick of the environmental variables cropped to the projection area.
+#' The second element is a raster of the projected model with the specified output type.
 #' @author Andrea Paz <paz.andreita@@gmail.com>
 #' @author Jamie Kass <jkass@@gradcenter.cuny.edu>
 #' @author Gonzalo E. Pinilla-Buitrago < gpinillabuitrago@@gradcenter.cuny.edu>
 # @note
-# @seealso
+#' @seealso \code{\link[dismo]{predict}}, \code{\link[ENMeval]{maxnet.predictRaster}}, \code{\link{proj_time}} \code{\link{proj_user}}
 # @references
 # @aliases - a list of additional topic names that will be mapped to
 # this documentation when the user looks them up from the command
@@ -57,9 +60,11 @@ proj_area <- function(evalOut, curModel, envs, outputType, alg, clamp, pjExt,
       modProjArea <- dismo::predict(evalOut@models[[curModel]], projMsk)
     } else if (alg == 'maxnet') {
       if (outputType == "raw") {pargs <- "exponential"} else {pargs <- outputType}
-      modProjArea <- ENMeval::maxnet.predictRaster(evalOut@models[[curModel]],
-                                                   projMsk, type = pargs,
-                                                   doClamp = clamp)
+      modProjArea <- ENMeval::enm.maxnet@pred(mod = evalOut@models[[curModel]],
+                                                   envs = projMsk, doClamp = clamp,
+                                                     pred.type = pargs)
+
+
     } else if (alg == 'maxent.jar') {
       pargs <- paste0("outputformat=", outputType)
       modProjArea <- dismo::predict(evalOut@models[[curModel]], projMsk,
@@ -69,3 +74,4 @@ proj_area <- function(evalOut, curModel, envs, outputType, alg, clamp, pjExt,
 
   return(list(projExt = projMsk, projArea = modProjArea))
 }
+
