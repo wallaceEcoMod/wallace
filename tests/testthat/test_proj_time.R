9e7d1b157abd90635a446946d4e0975c1ef262a1
Andrea Paz <andreapaz@Andreas-MacBook-Pro-2.local>
2020-06-23 16:54:43 -0500

    Generated the test and test passing

---
+++ tests/testthat/test_proj_time.R
@@ -0,0 +1,88 @@
+#### COMPONENT proj: Project Model
+#### MODULE: Project to New Extent
+context("proj_time")
+
+source("test_helper_functions.R")
+
+## occurrences
+out.gbif <- occs_queryDb(spName = "panthera onca", occDb = "gbif", occNum = 100)
+occs <- as.data.frame(out.gbif[[1]]$cleaned)
+## background mask
+# enviromental data
+envs <- envs_worldclim(bcRes = 10, bcSel = c('bio01','bio19'), doBrick = FALSE)
+# background extent
+bgExt <- penvs_bgExtent(occs, bgSel = 'bounding box', bgBuf = 0.5,spN=occs)
+# background masked
+bgMask <- penvs_bgMask(occs, envs, bgExt,spN=occs)
+## background sample
+bg <- penvs_bgSample(occs, bgMask, bgPtsNum = 10000,spN=occs)
+
+## Partition
+partblock <- part_partitionOccs(occs, bg, method = 'block', kfolds = NULL, bgMask = NULL,
+                                aggFact = NULL,spN=occs)
+# occurrences partitioned
+occsGrp = partblock$occ.grp
+# background points partitioned
+bgGrp = partblock$bg.grp
+
+## model
+# regularization multipliers
+rms <- c(1:2)
+# regularization multipliers step value
+rmsStep <- 1
+# feature classes
+fcs <- c('L', 'H', 'LQH')
+
+## extent to project
+# set coordinates
+longitude <- c(-71.58400, -78.81300, -79.34034, -69.83331, -66.47149, -66.71319, -71.11931)
+latitude <- c(13.18379, 7.52315, 0.93105, -1.70167, 0.98391, 6.09208, 12.74980)
+##projection time layers, using worldclim 2.1 Future 2021-2040 MIROC6 ssp126 bioclims as example
+envsFut<-list.files(path='./wc10/Future', pattern = ".tif$", full.names = TRUE)
+envsFut<-stack(envsFut)
+# generate matrix
+expertAddedPoly <- matrix(c(longitude, latitude), byrow = F, ncol = 2)
+###iterating items
+# outputType
+outputType <- c('raw', 'logistic', 'cloglog')
+# algorithm
+algorithm <- c('maxent.jar','maxnet','bioclim')
+# build model and test for both algorithms
+for (i in algorithm) {
+  if(i == 'bioclim'){
+    modAlg <- model_bioclim(occs, bg, occsGrp, bgGrp, bgMask,spN=occs)
+    curModel=1
+  }
+  else{
+    modAlg <- model_maxent(occs, bg, occsGrp, bgGrp, bgMask, rms, rmsStep, fcs, clampSel = TRUE,
+                           algMaxent = i,catEnvs=NULL,spN=occs)
+    curModel-'L_1'
+
+  }
+
+
+
+  for (j in outputType) {
+    ### run function
+    modProj <- proj_time(evalOut = modAlg, curModel, envs=envsFut, outputType = j,
+                         alg=i,clamp=FALSE, pjExt = expertAddedPoly )
+
+
+    ### test output features
+    test_that("output type checks", {
+      # the output is a list
+      expect_is(modProj, "list")
+      # the output list has five elements
+      expect_equal(length(modProj), 2)
+      # element within the output list are:
+      # a rasterBrick
+      expect_is(modProj$projExt, "RasterBrick")
+      # a rasterLayer
+      expect_is(modProj$projTime, "RasterLayer")
+      # there are as many projection extents as environmental variables used
+      expect_equal(raster::nlayers(envs), raster::nlayers(modProj$projExt))
+      # there is 1 projection area
+      expect_equal(raster::nlayers(modProj$projTime), 1)
+    })
+  }
+}
