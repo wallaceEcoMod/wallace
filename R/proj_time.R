a385f0db954867a6096c41522beb4aac9a5ea55f
Andrea Paz <andreapaz@Andreas-MacBook-Pro-2.local>
2020-06-23 17:27:42 -0500

    Added documentation

--- R/proj_time.R
+++ R/proj_time.R
@@ -1,27 +1,52 @@
 
 #' @title proj_time Project model to a new time
-#' @description ..
-#'
+#' @description Function projects the model generated in previous components to a new time and area using provided layers
+
 #' @details
-#' See Examples.
+#' This functions allows for the projection of the model created in previous components to a new time and area.
+#' The projection area is user provided in the map of the GUI and the projeciton time user selected. The model will be porjected to the new area and time as long as the environmental variables are available for the area.
+#' This function returns a list including the cropped environmental variables used for projecting and the projected model.
+
 #'
-#' @param evalOut x
-#' @param curModel x
-#' @param envs x
-#' @param outputType x
-#' @param pjExt x
-#' @param logger x
+#' @param evalOut ENMevaluate output from previous module and using any of the available algorithms
+#' @param curModel If algorithm is maxent, model selected by user as best or optimal, in terms of feature class and regularization multiplier (e.g 'L_1'). Else must be 1
+#' @param envs Environmental layers of different time to be used for projecting the model. They must match the layers used for generating the model in the model component
+#' @param outputType Output type to be used when algorithm is maxnet or maxent.jar.
+#' @param alg Modeling algorithm used in the model component. Can be one of : 'bioclim', 'maxent.jar' or 'maxnet'
+#' @param clamp logical, wether projection will be of clamped or uncalmped model.
+#' @param pjExt Extent of the area to project the model to. This is defined by the user in the map of the GUI and is provided as a matrix of latitude, longitude values.
+#' @param logger logger stores all notification messages to be displayed in the Log Window of Wallace GUI. insert the logger reactive list here for running in shiny,
+#'  otherwise leave the default NULL
 # @keywords
 #'
-# @examples
-#'
-#'
-# @return
+#' @examples
+#' out.gbif <- occs_queryDb(spName = "panthera onca", occDb = "gbif", occNum = 100)
+#' occs <- as.data.frame(out.gbif[[1]]$cleaned)
+#' envs <- envs_worldclim(bcRes = 10, bcSel = c('bio01','bio19'), doBrick = FALSE)
+#' bgExt <- penvs_bgExtent(occs, bgSel = 'bounding box', bgBuf = 0.5,spN=occs)
+#' bgMask <- penvs_bgMask(occs, envs, bgExt,spN=occs)
+#' bg <- penvs_bgSample(occs, bgMask, bgPtsNum = 10000,spN=occs)
+#' partblock <- part_partitionOccs(occs, bg, method = 'block', kfolds = NULL, bgMask = NULL,aggFact = NULL,spN=occs)
+#' ## extent to project
+#' longitude <- c(-71.58400, -78.81300, -79.34034, -69.83331, -66.47149, -66.71319, -71.11931)
+#' latitude <- c(13.18379, 7.52315, 0.93105, -1.70167, 0.98391, 6.09208, 12.74980)
+#' # generate matrix
+#' expertAddedPoly <- matrix(c(longitude, latitude), byrow = F, ncol = 2)
+#' ##projection time layers, using worldclim 2.1 Future 2021-2040 MIROC6 ssp126 bioclims as example
+#' envsFut<-list.files(path='./tests/testthat/wc10/Future', pattern = ".tif$", full.names = TRUE)
+#' envsFut<-stack(envsFut)
+#' modAlg <- model_bioclim(occs, bg, partblock$occ.grp, bgGrp = partblock$bg.grp, bgMask,spN=occs)
+#' modProj <- proj_time(evalOut = modAlg, curModel=1, envs=envsFut,alg='bioclim',clamp=FALSE, pjExt = expertAddedPoly )
+
+#' @return A list of two elements: projExt and projTime.
+#' The first is a RasterBrick or RasterStack of the environmental variables cropped to the projection area.
+#' The second element is a raster of the projected model with the specified output type.
 #' @author Andrea Paz <paz.andreita@@gmail.com>
 #' @author Jamie Kass <jkass@@gradcenter.cuny.edu>
 #' @author Gonzalo E. Pinilla-Buitrago < gpinillabuitrago@@gradcenter.cuny.edu>
 # @note
-# @seealso
+#' @seealso \code{\link[dismo]{predict}}, \code{\link[ENMeval]{maxnet.predictRaster}}, \code{\link{proj_area}} \code{\link{proj_user}}
+
 # @references
 # @aliases - a list of additional topic names that will be mapped to
 # this documentation when the user looks them up from the command
