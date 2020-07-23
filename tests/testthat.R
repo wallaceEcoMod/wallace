f715e63f65a2a802a40865026d79729f0260de8a
Andrea Paz <andreapaz@Andreas-MacBook-Pro-2.local>
2020-07-23 11:47:35 -0500

    For testing

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,9 @@
 library(testthat)
 library(wallace)
-
-# To run all tests from thest folder
-est_dir("~/tests/testthat/")
+#some tests need data so they must be run from the right testthat folder
+setwd("tests/testthat/")
+# To run all tests from tesst folder
+test_dir(".")
 
 ##this inside an R CMD can't get it to run yet
 test_check("wallace")
