e260b6b62e3d323b3e9563abf1f96cd96e5a7c2a
Gonzalo E. Pinilla-Buitrago <gpinillabuitrago@gradcenter.cuny.edu>
2020-08-17 13:38:08 -0500

    no warnings users shapefile

--- R/penvs_userBgExtent.R
+++ R/penvs_userBgExtent.R
@@ -56,7 +56,9 @@ penvs_userBgExtent <- function(bgShp_path, bgShp_name, userBgBuf, occs,
                           'following files: .shp, .shx, .dbf.'))
         return()
       }
-      file.rename(bgShp_path, file.path(pathdir, bgShp_name))
+      if (!file.exists(file.path(pathdir, bgShp_name)[1])) {
+        file.rename(bgShp_path, file.path(pathdir, bgShp_name))
+      }
       # get index of .shp
       i <- which(exts == 'shp')
       shpName <- strsplit(bgShp_name[i], '\\.')[[1]][1]
