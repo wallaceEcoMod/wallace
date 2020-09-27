0a016825dab0b471a94f639b39324ff8d79ac327
Gonzalo E. Pinilla-Buitrago <gpinillabuitrago@gradcenter.cuny.edu>
2020-09-27 12:12:58 -0500

    mask_expPoly UI

--- inst/shiny/modules/mask_expPoly.R
+++ inst/shiny/modules/mask_expPoly.R
@@ -1,8 +1,29 @@
 mask_expPoly_module_ui <- function(id) {
   ns <- shiny::NS(id)
   tagList(
-    # UI
-    actionButton(ns("run"), "Run module mask_expPoly")
+    span("Step 1:", class = "step"),
+    span("Choose Input Polygon (**)", class = "stepText"), br(), br(),
+    selectInput(ns('polyExpSel'), label = "Select method (**)",
+                choices = list("Draw polygon(**)" = 'expDraw',
+                               "User-specified polygon(**)" = 'expUser')),
+    conditionalPanel(sprintf("input['%s'] == 'expUser'", ns("polyExpSel")),
+                     fileInput(ns("polyExpShp"),
+                               label = paste0('Upload polygon in shapefile (.shp, .shx, .dbf) or ',
+                                              'CSV file with field order (longitude, latitude)'),
+                               accept = c(".csv", ".dbf", ".shx", ".shp"), multiple = TRUE)),
+    conditionalPanel(sprintf("input['%s'] == 'expDraw'", ns("polyExpSel")),
+                     p("Draw a polygon (**)")),
+    actionButton(ns("goInputPoly"), "Create(**)"), br(),
+    tags$hr(),
+    span("Step 2:", class = "step"),
+    span("Choose action (**)", class = "stepText"), br(),
+    p("Add or remove polygon (**)"),
+    tags$div(
+      title = "Add guidance text here (**)",
+      radioButtons(ns("actExpPoly"), "",
+                   choices = list("Add polygon" = 'addPoly',
+                                  "Remove polygon" = 'remPoly'), inline = TRUE),
+      actionButton(ns('goActionPoly'), "Mask (**)"))
   )
 }
 
