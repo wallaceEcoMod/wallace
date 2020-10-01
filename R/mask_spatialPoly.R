e29def1417f0e3ced77027859c1280d96a00bc8c
Gonzalo E. Pinilla-Buitrago <gpinillabuitrago@gradcenter.cuny.edu>
2020-10-01 18:51:33 -0500

    Step 1 mask_spatialPoly

---
+++ R/mask_spatialPoly.R
@@ -0,0 +1,71 @@
+#' @title mask_spatialPoly
+#' @description x
+#'
+#' @details
+#' x
+#'
+#' @param bgShp_path Path to the user provided shapefile
+#' @param bgShp_name Name of the user porvided shapefile
+#' @param bgExt x
+#' @param logger stores all notification messages to be displayed in the Log Window of Wallace GUI. insert the logger reactive list here for running in shiny,
+#' otherwise leave the default NULL
+#' @param spN x
+# @keywords
+
+# @keywords
+#'
+#' @return x
+#' @author Gonzalo E. Pinilla-Buitrago < gpinillabuitrago@@gradcenter.cuny.edu>
+# @note
+
+#' @export
+
+mask_spatialPoly <- function(bgShp_path, bgShp_name, bgExt,
+                            logger = NULL, spN = NULL) {
+  pathdir <- dirname(bgShp_path)
+  pathfile <- basename(bgShp_path)
+  # get extensions of all input files
+  exts <- sapply(strsplit(bgShp_name, '\\.'), FUN = function(x) x[2])
+  if ('shp' %in% exts) {
+    if (length(exts) < 3) {
+      logger %>%
+        writeLog(type = 'error', hlSpp(spN),
+                 paste0('If entering a shapefile, please select all the ',
+                        'following files: .shp, .shx, .dbf.'))
+      return()
+    }
+    # get index of .shp
+    i <- which(exts == 'shp')
+    if (!file.exists(file.path(pathdir, bgShp_name)[i])) {
+      file.rename(bgShp_path, file.path(pathdir, bgShp_name))
+    }
+    # read in shapefile and extract coords
+    polyData <- rgdal::readOGR(file.path(pathdir, bgShp_name)[i])
+  } else {
+    logger %>%
+      writeLog(type = 'error',
+               'Please enter shapefile (.shp, .shx, .dbf).')
+    return()
+  }
+
+  if (is.na(raster::crs(polyData, asText = TRUE))) {
+    logger %>% writeLog(
+      type = 'warning', hlSpp(spN),
+      "Projection not found for shapefile. It is assume that shapefile datum ",
+      "is WGS84 (**)"
+    )
+  }
+
+  if (!rgeos::gIntersects(bgExt, polyData)) {
+    logger %>% writeLog(
+      type = 'error', hlSpp(spN),
+      "Shapefile does not match with background extent. Please specify a new polygon. (**)"
+    )
+    return()
+  }
+
+  smartProgress(logger, message = "Uploading spatial data ...", {
+    spatialPoly <- rgeos::gIntersection(polyData, bgExt)
+  })
+  return(spatialPoly)
+}
--- inst/shiny/modules/mask_spatial.R
+++ inst/shiny/modules/mask_spatial.R
@@ -1,12 +1,45 @@
 mask_spatial_module_ui <- function(id) {
   ns <- shiny::NS(id)
   tagList(
-    # UI
-    actionButton(ns("run"), "Run module mask_spatial")
+    span("Step 1:", class = "step"),
+    span("Upload Spatial Data(**)", class = "stepText"), br(), br(),
+    fileInput(ns("maskShp"),
+              label = 'Upload polygon in shapefile (.shp, .shx, .dbf)',
+              accept = c(".dbf", ".shx", ".shp"), multiple = TRUE),
+    actionButton(ns("goMaskShp"), "Load (**)"), br(),
+    tags$hr()
   )
 }
 
 mask_spatial_module_server <- function(input, output, session, common) {
+
+  spp <- common$spp
+  curSp <- common$curSp
+  logger <- common$logger
+
+  observeEvent(input$goMaskShp, {
+    # WARNING ####
+    if (is.null(spp[[curSp()]]$postProc$prediction)) {
+      logger %>% writeLog(
+        type = 'error', hlSpp(curSp()), 'Calculate/Upload a model prediction (**)')
+      return()
+    }
+    if (is.null(input$maskShp$datapath)) {
+      logger %>% writeLog(
+        type = 'error', hlSpp(curSp()), "Specified filepath(s) (**)")
+      return()
+    }
+    # FUNCTION CALL ####
+    spatialMask <- mask_spatialPoly(input$maskShp$datapath, input$maskShp$name,
+                                    spp[[curSp()]]$procEnvs$bgExt,
+                                    logger, spN = curSp())
+    # LOAD INTO SPP ####
+    spp[[curSp()]]$mask$spatialMask <- spatialMask
+
+    logger %>% writeLog(hlSpp(curSp()), "Spatial data uploaded.")
+    # METADATA ####
+
+  })
 
   observeEvent(input$run, {
     # WARNING ####
@@ -47,9 +80,9 @@ mask_spatial_module_map <- function(map, common) {
 mask_spatial_module_rmd <- function(species) {
   # Variables used in the module's Rmd code
   list(
-    mask_spatial_knit = species$rmm$code$wallace$someFlag,
-    var1 = species$rmm$code$wallace$someSetting1,
-    var2 = species$rmm$code$wallace$someSetting2
+    mask_spatial_knit = FALSE
+    # var1 = species$rmm$code$wallace$someSetting1,
+    # var2 = species$rmm$code$wallace$someSetting2
   )
 }
 
