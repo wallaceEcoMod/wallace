927703beb953038f194acc1edab60f3ecbce3b23
Gonzalo E. Pinilla-Buitrago <gpinillabuitrago@gradcenter.cuny.edu>
2022-08-10 09:43:29 -0500

    [v1.9.3-9] Fix warning in occs_userOccs

--- R/occs_userOccs.R
+++ R/occs_userOccs.R
@@ -46,12 +46,11 @@ occs_userOccs <- function(txtPath, txtName, txtSep = ",", txtDec = ".",
   }
 
   # check to make sure all column names are correct
-  if (length(which(c('scientific_name', 'longitude', 'latitude') == names(txt))) != 3) {
+  if (sum(c('scientific_name', 'longitude', 'latitude') %in% names(txt)) != 3) {
     logger %>% writeLog(
       type = "error",
       paste0('Please input a file with columns "scientific_name", ',
-             '"longitude", "latitude" in that order or check delimeter and decimal ',
-             'separators. '))
+             '"longitude", "latitude". '))
     return()
   }
 
