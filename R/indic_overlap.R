3c74a17ecbdb97f909c2b008571883fe42972468
Gonzalo E. Pinilla-Buitrago <gpinillabuitrago@gradcenter.cuny.edu>
2022-08-24 14:58:19 -0500

    new indic_overlap()

---
+++ R/indic_overlap.R
@@ -0,0 +1,55 @@
+#' @title indic_overlap
+#' @description Overlap range map with shapefile or raster
+#' @param rangeMap sf polygon. A polygon with the range map.
+#' @param inputOverlap sf polygon or raster. A feature to apply overlap.
+#' @param field character. Name of the field to select categories/attributes.
+#' NULL is inputOverlap is a raster.
+#' @param category character. String with names of the categories/attributes
+#' selected to overlap with range map. NULL is inputOverlap is a raster.
+#' @param logger logger
+#' @param spN species name
+#' @author Gonzalo E. Pinilla-Buitrago <gpinillabuitrago@@gradcenter.cuny.edu>
+#' @export
+#'
+
+indic_overlap <- function(rangeMap, inputOverlap, field = NULL,
+                          category = NULL, logger = NULL, spN = NULL) {
+  if ("sf" %in% class(inputOverlap)) {
+    # GEPB: Uncomment when subfield in changeRangeR::ratioOverlap is working
+    # catAv <- unique(inputOverlap[[field]])
+    # if (length(catAv) == length(category)) {
+    #   categoryUse <- "All"
+    # } else {
+    #   categoryUse <- category
+    # }
+    smartProgress(
+      logger,
+      message = "Calculating range overlap ", {
+        sf::sf_use_s2(FALSE)
+        ratioOverlap <- changeRangeR::ratioOverlap(
+          r = sf::as_Spatial(rangeMap),
+          shp = sf::as_Spatial(inputOverlap),
+          field = field,
+          category = category)
+      }
+    )
+  } else if ("RasterLayer" %in% class(inputOverlap)) {
+    smartProgress(
+      logger,
+      message = "Calculating range overlap ", {
+        ratioOverlap <- changeRangeR::ratioOverlap(
+          r = sf::as_Spatial(rangeMap),
+          shp = inputOverlap)
+        # GEPB: Not sure when this scenario occurs. I get always a raster layer
+        # if (is.list(maskedRange) & length(maskedRange) > 1) {
+        #   names(maskedRange) <- NULL
+        #   maskedRange$fun <- mean
+        #   maskedRange$na.rm <- TRUE
+        #   maskedRange <- do.call(raster::mosaic, maskedRange)
+        # }
+      }
+    )
+  }
+  return(list(overlapRaster = ratioOverlap$maskedRange,
+              overlapRatio = ratioOverlap$ratio))
+}
