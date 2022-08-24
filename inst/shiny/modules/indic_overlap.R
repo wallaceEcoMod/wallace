fb8f5fb355d7e81269e404430b121af0f7dc103d
Gonzalo E. Pinilla-Buitrago <gpinillabuitrago@gradcenter.cuny.edu>
2022-08-24 15:00:15 -0500

    Map indic_overlap back

--- inst/shiny/modules/indic_overlap.R
+++ inst/shiny/modules/indic_overlap.R
@@ -363,176 +363,176 @@ indic_overlap_module_result <- function(id) {
 }
 
 indic_overlap_module_map <- function(map, common) {
-  # # Map logic
-  # spp <- common$spp
-  # curSp <- common$curSp
-  # map %>% clearAll()
-  # #if EOO is selected plot the polygon
-  # if (!is.null(spp[[curSp()]]$indic$overlapSourcePoly)) {
-  #
-  #   polyEOO <- spp[[curSp()]]$indic$EOOpoly@polygons[[1]]@Polygons
-  #   bb <- spp[[curSp()]]$indic$EOOpoly@bbox
-  #   bbZoom <- polyZoom(bb[1, 1], bb[2, 1], bb[1, 2], bb[2, 2], fraction = 0.05)
-  #   map %>%
-  #     fitBounds(bbZoom[1], bbZoom[2], bbZoom[3], bbZoom[4])
-  #   map %>%
-  #     ##Add legend
-  #     addLegend("bottomright", colors = "gray",
-  #               title = "EOO", labels = "EOO",
-  #               opacity = 1)
-  #   ##ADD polygon
-  #   if (length(polyEOO) == 1) {
-  #     xy <- list(polyEOO[[1]]@coords)
-  #   } else {
-  #     xy <- lapply(polyEOO, function(x) x@coords)
-  #   }
-  #   for (shp in xy) {
-  #     map %>%
-  #       addPolygons(lng = shp[, 1], lat = shp[, 2], weight = 4, color = "gray",
-  #                   group = 'indic')
-  #   }
-  # }
-  #
-  # #plot SDM to use
-  # if (is.null(spp[[curSp()]]$indic$overlapSourcePoly)) {
-  #   req(spp[[curSp()]]$indic$overlapSourcePoly)
-  #   sdm <-  spp[[curSp()]]$indic$overlapSourcePoly
-  #   raster::crs(sdm) <- sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 ")
-  #   SDMVals <- getRasterVals(sdm)
-  #   rasCols <- c("#2c7bb6", "#abd9e9", "#ffffbf", "#fdae61", "#d7191c")
-  #   legendPal <- colorNumeric(rev(rasCols), SDMVals, na.color = 'transparent')
-  #   rasPal <- colorNumeric(rasCols, SDMVals, na.color = 'transparent')
-  #   zoomExt <- raster::extent(sdm)
-  #   map %>% fitBounds(lng1 = zoomExt[1], lng2 = zoomExt[2],
-  #                     lat1 = zoomExt[3], lat2 = zoomExt[4])
-  #   if (length(unique(SDMVals)) == 3 | length(unique(SDMVals)) == 2) {
-  #     map %>%
-  #       addLegend("bottomright", colors = c('red', 'grey'),
-  #                 title = "SDM",
-  #                 labels = c("Presence", "Absence"),
-  #                 opacity = 1, layerId = 'sdm') %>%
-  #       addRasterImage(sdm, colors = c('grey', 'red'),
-  #                      opacity = 0.7, group = 'indic', layerId = 'sdm',
-  #                      method = "ngb")
-  #   } else if (length(unique(SDMVals)) == 1) {
-  #     map %>%
-  #       addLegend("bottomright", colors = 'red',
-  #                 title = "AOO",
-  #                 labels = "Presence",
-  #                 opacity = 1, layerId = 'expert') %>%
-  #       addRasterImage(sdm, colors = 'red',
-  #                      opacity = 0.7, group = 'indic', layerId = 'Overlap',
-  #                      method = "ngb")
-  #   } else {
-  #     # if no threshold specified
-  #     legendPal <- colorNumeric(rev(rasCols), SDMVals, na.color = 'transparent')
-  #     rasPal <- colorNumeric(rasCols, SDMVals, na.color = 'transparent')
-  #     map %>%
-  #       addLegend("bottomright", pal = legendPal, title = "SDM",
-  #                 values = SDMVals, layerId = "sdm",
-  #                 labFormat = reverseLabel(2, reverse_order=TRUE)) %>%
-  #       addRasterImage(sdm, colors = rasPal,
-  #                      opacity = 0.7, group = 'indic', layerId = 'sdm',
-  #                      method = "ngb")
-  #   }
-  # }
-  # # Add just projection Polygon
-  # req(spp[[curSp()]]$indic$polyOverlap)
-  # polyOvXY <- spp[[curSp()]]$indic$polyOverlap@polygons
-  # if(length(polyOvXY) == 1) {
-  #   shp <- list(polyOvXY[[1]]@Polygons[[1]]@coords)
-  # } else {
-  #   shp <- lapply(polyOvXY, function(x) x@Polygons[[1]]@coords)
-  # }
-  # bb <- spp[[curSp()]]$indic$polyOverlap@bbox
-  # bbZoom <- polyZoom(bb[1, 1], bb[2, 1], bb[1, 2], bb[2, 2], fraction = 0.05)
-  # map %>%
-  #   fitBounds(bbZoom[1], bbZoom[2], bbZoom[3], bbZoom[4])
-  # for (poly in shp) {
-  #   map %>% addPolygons(lng = poly[, 1], lat = poly[, 2], weight = 4,
-  #                       color = "red", fill=FALSE, group = 'indic')
-  # }
-  #
-  # ##Plot overlap of polygons (EOO case)
-  # if(!is.null(spp[[curSp()]]$indic$overlapPoly)){
-  #   req(spp[[curSp()]]$indic$overlapPoly)
-  #   polyOver <- as_Spatial(spp[[curSp()]]$indic$overlapPoly)
-  #   bb <- polyOver@bbox
-  #   polyOver <- polyOver@polygons[[1]]@Polygons
-  #
-  #   bbZoom <- polyZoom(bb[1, 1], bb[2, 1], bb[1, 2], bb[2, 2], fraction = 0.05)
-  #   map %>%
-  #     fitBounds(bbZoom[1], bbZoom[2], bbZoom[3], bbZoom[4])
-  #   map %>%
-  #     ##Add legend
-  #     addLegend("bottomright", colors = "red",
-  #               title = "Overlap", labels = "Overlap",
-  #               opacity = 1)
-  #   ##ADD polygon
-  #   if (length(polyOver) == 1) {
-  #     xy <- list(polyOver[[1]]@coords)
-  #   } else {
-  #     xy <- lapply(polyOver, function(x) x@coords)
-  #   }
-  #   for (shp in xy) {
-  #     map %>%
-  #       addPolygons(lng = shp[, 1], lat = shp[, 2], weight = 4, color = "red",
-  #                   group = 'indic')
-  #   }
-  # }
-  # ##Plot overlap of raster vs raster (code to get unclear)
-  # ##Plot overlap of poly and raster (SDM vs. polygon case)
-  # if (!is.null(spp[[curSp()]]$indic$overlapRaster)) {
-  #   req(spp[[curSp()]]$indic$overlapRaster)
-  #   Overlap <-  spp[[curSp()]]$indic$overlapRaster
-  #   #  if(is.list(Overlap)){
-  #   # Overlap$fun <- mean
-  #   #Overlap$na.rm <- TRUE
-  #
-  #   #Overlap <- do.call(raster::mosaic, Overlap)
-  #   #  }
-  #   raster::crs(Overlap) <- sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 ")
-  #   OverlapVals <- spp[[curSp()]]$indic$overlapvalues
-  #   rasCols <- c("#2c7bb6", "#abd9e9", "#ffffbf", "#fdae61", "#d7191c")
-  #   legendPal <- colorNumeric(rev(rasCols), OverlapVals, na.color = 'transparent')
-  #   rasPal <- colorNumeric(rasCols, OverlapVals, na.color = 'transparent')
-  #   zoomExt <- raster::extent(Overlap)
-  #   map %>% fitBounds(lng1 = zoomExt[1], lng2 = zoomExt[2],
-  #                     lat1 = zoomExt[3], lat2 = zoomExt[4])
-  #   # Create legend
-  #   map %>% clearAll()
-  #   if (length(unique(OverlapVals)) == 3 |
-  #       length(unique(OverlapVals)) == 2) {
-  #     map %>%
-  #       addLegend("bottomright", colors = c('red', 'grey'),
-  #                 title = "Range Overlap",
-  #                 labels = c("Presence", "Absence"),
-  #                 opacity = 1, layerId = 'expert') %>%
-  #       addRasterImage(Overlap, colors = c('gray', 'red'),
-  #                      opacity = 0.7, group = 'indic', layerId = 'Overlap',
-  #                      method = "ngb")
-  #   } else if (length(unique(OverlapVals)) == 1) {
-  #     map %>%
-  #       addLegend("bottomright", colors = 'red',
-  #                 title = "Range Overlap",
-  #                 labels = "Presence",
-  #                 opacity = 1, layerId = 'expert') %>%
-  #       addRasterImage(Overlap, colors = 'red',
-  #                      opacity = 0.7, group = 'indic', layerId = 'Overlap',
-  #                      method = "ngb")
-  #   } else {
-  #     # if threshold specified
-  #     legendPal <- colorNumeric(rev(rasCols), OverlapVals, na.color = 'transparent')
-  #     rasPal <- colorNumeric(rasCols, OverlapVals, na.color = 'transparent')
-  #     map %>%
-  #       addLegend("bottomright", pal = legendPal, title = "Range Overlap",
-  #                 values = OverlapVals, layerId = "overlap",
-  #                 labFormat = reverseLabel(2, reverse_order=TRUE)) %>%
-  #       addRasterImage(Overlap, colors = rasPal,
-  #                      opacity = 0.7, group = 'indic', layerId = 'Overlap',
-  #                      method = "ngb")
-  #   }
-  # }
+  # Map logic
+  spp <- common$spp
+  curSp <- common$curSp
+  map %>% clearAll()
+  #if EOO is selected plot the polygon
+  if (!is.null(spp[[curSp()]]$indic$overlapSourcePoly)) {
+
+    polyEOO <- spp[[curSp()]]$indic$EOOpoly@polygons[[1]]@Polygons
+    bb <- spp[[curSp()]]$indic$EOOpoly@bbox
+    bbZoom <- polyZoom(bb[1, 1], bb[2, 1], bb[1, 2], bb[2, 2], fraction = 0.05)
+    map %>%
+      fitBounds(bbZoom[1], bbZoom[2], bbZoom[3], bbZoom[4])
+    map %>%
+      ##Add legend
+      addLegend("bottomright", colors = "gray",
+                title = "EOO", labels = "EOO",
+                opacity = 1)
+    ##ADD polygon
+    if (length(polyEOO) == 1) {
+      xy <- list(polyEOO[[1]]@coords)
+    } else {
+      xy <- lapply(polyEOO, function(x) x@coords)
+    }
+    for (shp in xy) {
+      map %>%
+        addPolygons(lng = shp[, 1], lat = shp[, 2], weight = 4, color = "gray",
+                    group = 'indic')
+    }
+  }
+
+  #plot SDM to use
+  if (is.null(spp[[curSp()]]$indic$overlapSourcePoly)) {
+    req(spp[[curSp()]]$indic$overlapSourcePoly)
+    sdm <-  spp[[curSp()]]$indic$overlapSourcePoly
+    raster::crs(sdm) <- sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 ")
+    SDMVals <- getRasterVals(sdm)
+    rasCols <- c("#2c7bb6", "#abd9e9", "#ffffbf", "#fdae61", "#d7191c")
+    legendPal <- colorNumeric(rev(rasCols), SDMVals, na.color = 'transparent')
+    rasPal <- colorNumeric(rasCols, SDMVals, na.color = 'transparent')
+    zoomExt <- raster::extent(sdm)
+    map %>% fitBounds(lng1 = zoomExt[1], lng2 = zoomExt[2],
+                      lat1 = zoomExt[3], lat2 = zoomExt[4])
+    if (length(unique(SDMVals)) == 3 | length(unique(SDMVals)) == 2) {
+      map %>%
+        addLegend("bottomright", colors = c('red', 'grey'),
+                  title = "SDM",
+                  labels = c("Presence", "Absence"),
+                  opacity = 1, layerId = 'sdm') %>%
+        addRasterImage(sdm, colors = c('grey', 'red'),
+                       opacity = 0.7, group = 'indic', layerId = 'sdm',
+                       method = "ngb")
+    } else if (length(unique(SDMVals)) == 1) {
+      map %>%
+        addLegend("bottomright", colors = 'red',
+                  title = "AOO",
+                  labels = "Presence",
+                  opacity = 1, layerId = 'expert') %>%
+        addRasterImage(sdm, colors = 'red',
+                       opacity = 0.7, group = 'indic', layerId = 'Overlap',
+                       method = "ngb")
+    } else {
+      # if no threshold specified
+      legendPal <- colorNumeric(rev(rasCols), SDMVals, na.color = 'transparent')
+      rasPal <- colorNumeric(rasCols, SDMVals, na.color = 'transparent')
+      map %>%
+        addLegend("bottomright", pal = legendPal, title = "SDM",
+                  values = SDMVals, layerId = "sdm",
+                  labFormat = reverseLabel(2, reverse_order=TRUE)) %>%
+        addRasterImage(sdm, colors = rasPal,
+                       opacity = 0.7, group = 'indic', layerId = 'sdm',
+                       method = "ngb")
+    }
+  }
+  # Add just projection Polygon
+  req(spp[[curSp()]]$indic$polyOverlap)
+  polyOvXY <- spp[[curSp()]]$indic$polyOverlap@polygons
+  if(length(polyOvXY) == 1) {
+    shp <- list(polyOvXY[[1]]@Polygons[[1]]@coords)
+  } else {
+    shp <- lapply(polyOvXY, function(x) x@Polygons[[1]]@coords)
+  }
+  bb <- spp[[curSp()]]$indic$polyOverlap@bbox
+  bbZoom <- polyZoom(bb[1, 1], bb[2, 1], bb[1, 2], bb[2, 2], fraction = 0.05)
+  map %>%
+    fitBounds(bbZoom[1], bbZoom[2], bbZoom[3], bbZoom[4])
+  for (poly in shp) {
+    map %>% addPolygons(lng = poly[, 1], lat = poly[, 2], weight = 4,
+                        color = "red", fill=FALSE, group = 'indic')
+  }
+
+  ##Plot overlap of polygons (EOO case)
+  if(!is.null(spp[[curSp()]]$indic$overlapPoly)){
+    req(spp[[curSp()]]$indic$overlapPoly)
+    polyOver <- as_Spatial(spp[[curSp()]]$indic$overlapPoly)
+    bb <- polyOver@bbox
+    polyOver <- polyOver@polygons[[1]]@Polygons
+
+    bbZoom <- polyZoom(bb[1, 1], bb[2, 1], bb[1, 2], bb[2, 2], fraction = 0.05)
+    map %>%
+      fitBounds(bbZoom[1], bbZoom[2], bbZoom[3], bbZoom[4])
+    map %>%
+      ##Add legend
+      addLegend("bottomright", colors = "red",
+                title = "Overlap", labels = "Overlap",
+                opacity = 1)
+    ##ADD polygon
+    if (length(polyOver) == 1) {
+      xy <- list(polyOver[[1]]@coords)
+    } else {
+      xy <- lapply(polyOver, function(x) x@coords)
+    }
+    for (shp in xy) {
+      map %>%
+        addPolygons(lng = shp[, 1], lat = shp[, 2], weight = 4, color = "red",
+                    group = 'indic')
+    }
+  }
+  ##Plot overlap of raster vs raster (code to get unclear)
+  ##Plot overlap of poly and raster (SDM vs. polygon case)
+  if (!is.null(spp[[curSp()]]$indic$overlapRaster)) {
+    req(spp[[curSp()]]$indic$overlapRaster)
+    Overlap <-  spp[[curSp()]]$indic$overlapRaster
+    #  if(is.list(Overlap)){
+    # Overlap$fun <- mean
+    #Overlap$na.rm <- TRUE
+
+    #Overlap <- do.call(raster::mosaic, Overlap)
+    #  }
+    raster::crs(Overlap) <- sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 ")
+    OverlapVals <- spp[[curSp()]]$indic$overlapvalues
+    rasCols <- c("#2c7bb6", "#abd9e9", "#ffffbf", "#fdae61", "#d7191c")
+    legendPal <- colorNumeric(rev(rasCols), OverlapVals, na.color = 'transparent')
+    rasPal <- colorNumeric(rasCols, OverlapVals, na.color = 'transparent')
+    zoomExt <- raster::extent(Overlap)
+    map %>% fitBounds(lng1 = zoomExt[1], lng2 = zoomExt[2],
+                      lat1 = zoomExt[3], lat2 = zoomExt[4])
+    # Create legend
+    map %>% clearAll()
+    if (length(unique(OverlapVals)) == 3 |
+        length(unique(OverlapVals)) == 2) {
+      map %>%
+        addLegend("bottomright", colors = c('red', 'grey'),
+                  title = "Range Overlap",
+                  labels = c("Presence", "Absence"),
+                  opacity = 1, layerId = 'expert') %>%
+        addRasterImage(Overlap, colors = c('gray', 'red'),
+                       opacity = 0.7, group = 'indic', layerId = 'Overlap',
+                       method = "ngb")
+    } else if (length(unique(OverlapVals)) == 1) {
+      map %>%
+        addLegend("bottomright", colors = 'red',
+                  title = "Range Overlap",
+                  labels = "Presence",
+                  opacity = 1, layerId = 'expert') %>%
+        addRasterImage(Overlap, colors = 'red',
+                       opacity = 0.7, group = 'indic', layerId = 'Overlap',
+                       method = "ngb")
+    } else {
+      # if threshold specified
+      legendPal <- colorNumeric(rev(rasCols), OverlapVals, na.color = 'transparent')
+      rasPal <- colorNumeric(rasCols, OverlapVals, na.color = 'transparent')
+      map %>%
+        addLegend("bottomright", pal = legendPal, title = "Range Overlap",
+                  values = OverlapVals, layerId = "overlap",
+                  labFormat = reverseLabel(2, reverse_order=TRUE)) %>%
+        addRasterImage(Overlap, colors = rasPal,
+                       opacity = 0.7, group = 'indic', layerId = 'Overlap',
+                       method = "ngb")
+    }
+  }
 }
 
 indic_overlap_module_rmd <- function(species) {
