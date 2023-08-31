14245f04b16f383d7f8eaba6e7e329c5986ba355
Bethany Johnson <bjohnso005@citymail.cuny.edu>
2023-08-31 11:25:39 -0400

    temporarily removed paleobioDb (pkg off of CRAN)

--- R/occs_paleoDb.R
+++ R/occs_paleoDb.R
@@ -32,61 +32,61 @@
 #' }
 #' @export
 
-occs_paleoDb <- function(spName, occNum, timeInterval, logger = NULL) {
-  if (!requireNamespace("paleobioDB", quietly = TRUE)) {
-    logger %>%
-      writeLog(
-        type = "warning",
-        "This module is available if you install the 'paleobioDB' package ",
-        "(which is a suggested package for Wallace, not a required dependency). If you ",
-        "want to install it, close Wallace and run the following line in the ",
-        "R Console: ", em("install.packages('paleobioDB')")
-      )
-    return()
-  }
-  spName <- trimws(spName)
-  # figure out how many separate names (components of scientific name) were entered
-  nameSplit <- length(unlist(strsplit(spName, " ")))
-  # if two names not entered, throw error and return
-  if (nameSplit != 2) {
-    logger %>% writeLog(type = 'error',
-      'Please input both genus and species names of ONE species.')
-    return()
-  }
-  spName <- paste0(toupper(substring(spName, 1, 1)),
-                   substring(spName, 2, nchar(spName)))
-  smartProgress(logger, message = paste0("Querying paleobioDB ..."), {
-    occsOrig <- try(paleobioDB::pbdb_occurrences(taxon_name = spName,
-                                                 limit = occNum,
-                                                 interval = timeInterval,
-                                                 vocab = 'pbdb',
-                                                 show = c("coords", "bin", "loc")),
-                    silent = TRUE)
-  })
-
-  if (inherits(occsOrig, "try-error")) {
-    logger %>% writeLog(
-      type = 'error',
-      hlSpp(hlSpp(fmtSpN(spName))),
-      "No records found, please check the spelling.")
-    return()
-  }
-
-  occsOrig <- dplyr::as_tibble(occsOrig)
-  occsOrig$lng <- as.numeric(occsOrig$lng)
-  occsOrig$lat <- as.numeric(occsOrig$lat)
-  # get total number of records found in database
-  totRows <- nrow(occsOrig)
-  # extract occurrence tibble
-  names(occsOrig)[names(occsOrig) == "lng"] <- "longitude"
-  names(occsOrig)[names(occsOrig) == "lat"] <- "latitude"
-  names(occsOrig)[names(occsOrig) == "cc"] <- "country"
-  occsOrig$taxon_name <- as.character(occsOrig$taxon_name)
-  names(occsOrig)[names(occsOrig) == "taxon_name"] <- "scientific_name"
-
-  # make new column for original ID
-  occsOrig$occID <- 1:nrow(occsOrig)
-
+# occs_paleoDb <- function(spName, occNum, timeInterval, logger = NULL) {
+#  if (!requireNamespace("paleobioDB", quietly = TRUE)) {
+#    logger %>%
+#      writeLog(
+#        type = "warning",
+#        "This module is available if you install the 'paleobioDB' package ",
+#        "(which is a suggested package for Wallace, not a required dependency). If you ",
+#        "want to install it, close Wallace and run the following line in the ",
+#        "R Console: ", em("install.packages('paleobioDB')")
+#     )
+#    return()
+#  }
+#  spName <- trimws(spName)
+#  # figure out how many separate names (components of scientific name) were entered
+#  nameSplit <- length(unlist(strsplit(spName, " ")))
+#  # if two names not entered, throw error and return
+#  if (nameSplit != 2) {
+#    logger %>% writeLog(type = 'error',
+#      'Please input both genus and species names of ONE species.')
+#    return()
+#  }
+#  spName <- paste0(toupper(substring(spName, 1, 1)),
+#                   substring(spName, 2, nchar(spName)))
+#  smartProgress(logger, message = paste0("Querying paleobioDB ..."), {
+#    occsOrig <- try(paleobioDB::pbdb_occurrences(taxon_name = spName,
+#                                                 limit = occNum,
+#                                                 interval = timeInterval,
+#                                                 vocab = 'pbdb',
+#                                                 show = c("coords", "bin", "loc")),
+#                    silent = TRUE)
+#  })
+#
+#  if (inherits(occsOrig, "try-error")) {
+#    logger %>% writeLog(
+#      type = 'error',
+#      hlSpp(hlSpp(fmtSpN(spName))),
+#      "No records found, please check the spelling.")
+#    return()
+#  }
+#
+#  occsOrig <- dplyr::as_tibble(occsOrig)
+#  occsOrig$lng <- as.numeric(occsOrig$lng)
+#  occsOrig$lat <- as.numeric(occsOrig$lat)
+#  # get total number of records found in database
+#  totRows <- nrow(occsOrig)
+#  # extract occurrence tibble
+#  names(occsOrig)[names(occsOrig) == "lng"] <- "longitude"
+#  names(occsOrig)[names(occsOrig) == "lat"] <- "latitude"
+#  names(occsOrig)[names(occsOrig) == "cc"] <- "country"
+#  occsOrig$taxon_name <- as.character(occsOrig$taxon_name)
+#  names(occsOrig)[names(occsOrig) == "taxon_name"] <- "scientific_name"
+#
+#  # make new column for original ID
+#  occsOrig$occID <- 1:nrow(occsOrig)
+#
   # subset to just records with latitude and longitude
   # all plaeobioDB recors have coords, so this warning is commented until future database
   # occsXY <-  occsOrig[!is.na(occsOrig$longitude) & !is.na(occsOrig$latitude),]
@@ -95,30 +95,30 @@ occs_paleoDb <- function(spName, occNum, timeInterval, logger = NULL) {
   #     type = 'warning',
   #     hlSpp(spName), "No records with coordinates found in paleobioDB.")
   # }
-  occsXY <- occsOrig
-
-  dups <- duplicated(occsXY[,c('longitude','latitude')])
-  occs <- occsXY[!dups, ]
-
-  # subset by key columns and make id and popup columns
-  cols <- c("occID", "scientific_name", "longitude", "latitude",
-            # "early_interval", "late_interval",
-            "country", "collection_no", "record_type",
-            "early_age", "late_age")
-  occs <- occs %>% dplyr::select(dplyr::one_of(cols)) %>%
-    # make new column for leaflet marker popup content
-    dplyr::mutate(pop = unlist(apply(occs, 1, popUpContent))) %>%
-    dplyr::arrange(dplyr::across(cols))
-  occs$early_age <- as.numeric(occs$early_age)
-  occs$late_age <- as.numeric(occs$late_age)
-  noCoordsRem <- nrow(occsOrig) - nrow(occsXY)
-
-  dupsRem <- nrow(occsXY) - nrow(occs)
-  logger %>% writeLog(
-    hlSpp(fmtSpN(spName)),
-    'Total paleobioDb records returned [', nrow(occsOrig), '] (limit ', occNum,
-    '). Records without coordinates removed [',
-    noCoordsRem, ']. Duplicated records removed [', dupsRem,
-    ']. Remaining records [', nrow(occs), '].')
-  return(list(orig = occsOrig, cleaned = as.data.frame(occs)))
-}
+#  occsXY <- occsOrig
+#
+#  dups <- duplicated(occsXY[,c('longitude','latitude')])
+#  occs <- occsXY[!dups, ]
+#
+#  # subset by key columns and make id and popup columns
+#  cols <- c("occID", "scientific_name", "longitude", "latitude",
+#            # "early_interval", "late_interval",
+#            "country", "collection_no", "record_type",
+#            "early_age", "late_age")
+#  occs <- occs %>% dplyr::select(dplyr::one_of(cols)) %>%
+#    # make new column for leaflet marker popup content
+#    dplyr::mutate(pop = unlist(apply(occs, 1, popUpContent))) %>%
+#    dplyr::arrange(dplyr::across(cols))
+#  occs$early_age <- as.numeric(occs$early_age)
+#  occs$late_age <- as.numeric(occs$late_age)
+#  noCoordsRem <- nrow(occsOrig) - nrow(occsXY)
+#
+#  dupsRem <- nrow(occsXY) - nrow(occs)
+#  logger %>% writeLog(
+#    hlSpp(fmtSpN(spName)),
+#    'Total paleobioDb records returned [', nrow(occsOrig), '] (limit ', occNum,
+#    '). Records without coordinates removed [',
+#    noCoordsRem, ']. Duplicated records removed [', dupsRem,
+#    ']. Remaining records [', nrow(occs), '].')
+#  return(list(orig = occsOrig, cleaned = as.data.frame(occs)))
+#}
