5527f0d2e638f6696831fd672ff94ad48ef0c1a4
Bethany Johnson <bjohnso005@citymail.cuny.edu>
2024-10-18 17:18:44 -0400

    added license & # REFERENCES (knitcitations) to v3 modules

--- inst/shiny/modules/diver_endemism.R
+++ inst/shiny/modules/diver_endemism.R
@@ -1,3 +1,26 @@
+# Wallace EcoMod: a flexible platform for reproducible modeling of
+# species niches and distributions.
+#
+# diver_endemism.R
+# File author: Wallace EcoMod Dev Team. 2023.
+# --------------------------------------------------------------------------
+# This file is part of the Wallace EcoMod application
+# (hereafter “Wallace”).
+#
+# Wallace is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# Wallace is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Wallace. If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------
+#
 diver_endemism_module_ui <- function(id) {
   ns <- shiny::NS(id)
   tagList(
@@ -140,10 +163,18 @@ diver_endemism_module_server <- function(input, output, session, common) {
     # FUNCTION CALL ####
     req(endemism)
     logger %>% writeLog("Species endemism calculated.")
+
     # LOAD INTO SPP ####
     multi.sp$endemism <- endemism
     multi.sp$sppEndemism <- curSp()
     common$update_component(tab = "Map")
+
+    # REFERENCES ####
+    knitcitations::citep(citation("raster"))
+    knitcitations::citep(citation("changeRangeR"))
+
+    # METADATA ####
+    # add metadata
   })
 
   output$result <- renderPrint({
--- inst/shiny/modules/mask_spatial.R
+++ inst/shiny/modules/mask_spatial.R
@@ -1,3 +1,26 @@
+# Wallace EcoMod: a flexible platform for reproducible modeling of
+# species niches and distributions.
+#
+# mask_spatial.R
+# File author: Wallace EcoMod Dev Team. 2023.
+# --------------------------------------------------------------------------
+# This file is part of the Wallace EcoMod application
+# (hereafter “Wallace”).
+#
+# Wallace is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# Wallace is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Wallace. If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------
+#
 mask_spatial_module_ui <- function(id) {
   ns <- shiny::NS(id)
   tagList(
@@ -190,7 +213,12 @@ mask_spatial_module_server <- function(input, output, session, common) {
     logger %>% writeLog(
       hlSpp(curSp()), "Spatial Masked (**)")
 
+    # REFERENCES ####
+    knitcitations::citep(citation("sp"))
+    knitcitations::citep(citation("sf"))
+
     # METADATA ####
+    #add metadata
   })
 
   # Reset prediction
