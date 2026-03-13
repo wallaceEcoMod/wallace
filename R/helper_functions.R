4445879614b50cea73e0089b8ac1eb389a573ab9
Daniel Lopez <dfloplo@gmail.com>
2026-03-13 14:31:52 -0400

    Adjust the draw toolbar removal logic

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -186,7 +186,7 @@ writeLog <- function(logger, ..., type = 'default') {
 #' @export
 clearAll <- function(map) {
   map %>% clearMarkers() %>% clearShapes() %>% clearImages() %>%
-    clearControls() %>% removeLayersControl()
+    clearControls() %>% removeLayersControl() %>% removeDrawToolbar()
 }
 
 #' @title polyZoom
